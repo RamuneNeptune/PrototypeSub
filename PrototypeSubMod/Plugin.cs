976c8bb0432d086afcb9da03e7627e4287e94471
Indigocoder1 <caleb@liston.us>
2024-07-06 11:33:42 -0700

    Added VS project

---
+++ PrototypeSubMod/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace PrototypeSubMod
+{
+    [BepInPlugin(GUID, pluginName, versionString)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string GUID = "com.prototypeteam.prototypesub";
+        private const string pluginName = "Prototype Sub";
+        private const string versionString = "1.0.0";
+
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // Set project-scoped logger instance
+            Logger = base.Logger;
+
+            // Register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{GUID}");
+            Logger.LogInfo($"Plugin {GUID} is loaded!");
+        }
+    }
+}
