a2f8e216b545c311a964d6c4bcf7f7f9273da708
Indigocoder1 <caleb@liston.us>
2024-07-20 14:39:51 -0700

    Added engine rpm sfx manager

---
+++ PrototypeSubMod/Patches/ExternalDamageManagerPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.Prefabs;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(CyclopsExternalDamageManager))]
+internal class ExternalDamageManagerPatches
+{
+    [HarmonyPatch(nameof(CyclopsExternalDamageManager.OnEnable)), HarmonyPrefix]
+    private static bool OnEnable_Prefix(CyclopsExternalDamageManager __instance)
+    {
+        var techTag = __instance.subRoot.GetComponent<TechTag>();
+        if (techTag.type == Prototype_Craftable.SubInfo.TechType) return false;
+
+        return true;
+    }
+}
