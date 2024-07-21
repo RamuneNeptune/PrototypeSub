e54e1be5db5372c7798f8ffbe646e7a987fdf50c
Indigocoder1 <caleb@liston.us>
2024-07-21 12:01:38 -0700

    Tuning piloting animations

---
+++ PrototypeSubMod/Monobehaviors/PlayerPilotingAnimationSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class PlayerPilotingAnimationSetter : MonoBehaviour
+{
+    [SerializeField] private PilotingChair chair;
+    [SerializeField] private string parameterName;
+
+    private bool handDownRecently;
+
+    public void OnCinematicStarted()
+    {
+        //cyclops_ladder_short_down
+        Player.main.playerAnimator.SetBool(parameterName, chair.currentPlayer == Player.main || handDownRecently);
+    }
+
+    //Called by CinematicModeTriggerBase via SendMessage
+    public void HandDown()
+    {
+        handDownRecently = true;
+
+        Invoke(nameof(ResetHandDown), 1f);
+    }
+
+    private void ResetHandDown()
+    {
+        handDownRecently = false;
+    }
+}
