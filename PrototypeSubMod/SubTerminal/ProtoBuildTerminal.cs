ba001c4c5e010695e7626a6c84823b9319bc3dcc
Indigocoder1 <caleb@liston.us>
2024-07-30 14:09:27 -0700

    Added build terminal scripts

---
+++ PrototypeSubMod/SubTerminal/ProtoBuildTerminal.cs
@@ -0,0 +1,25 @@
+﻿using PrototypeSubMod.PowerSystem;
+using SubLibrary.SaveData;
+using System.Linq;
+using UnityEngine;
+
+namespace PrototypeSubMod.SubTerminal;
+
+internal class ProtoBuildTerminal : MonoBehaviour
+{
+    public bool HasBuiltProtoSub
+    {
+        get
+        {
+            return prototypeSub != null;
+        }
+    }
+
+    private GameObject prototypeSub;
+
+    private void Start()
+    {
+        var serializationManagers = FindObjectsOfType<SubSerializationManager>();
+        prototypeSub = serializationManagers.FirstOrDefault(s => s.GetComponentInChildren<PrototypePowerSystem>() != null)?.gameObject;
+    }
+}
---
+++ PrototypeSubMod/SubTerminal/uGUI_ProtoBuildScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace PrototypeSubMod.SubTerminal;
+
+internal class uGUI_ProtoBuildScreen : MonoBehaviour
+{
+    public event Action OnBuildStarted;
+
+    [SerializeField] private RocketBuilderTooltip tooltip;
+    [SerializeField] private TextMeshProUGUI constructButtonText;
+    [SerializeField] private PlayerDistanceTracker distanceTracker;
+
+    private void Start()
+    {
+        constructButtonText.text = Language.main.Get("ConstructButton");
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public void OnConstructPressed()
+    {
+        OnBuildStarted?.Invoke();
+    }
+
+    private void UpdateTooltipActive()
+    {
+
+    }
+}
