7426ec80a7ad6f9cdbafd2f6628e0602d3262454
Indigocoder1 <caleb@liston.us>
2024-08-08 20:52:26 -0700

    Added fire system

---
+++ PrototypeSubMod/Monobehaviors/FirePrefabSetter.cs
@@ -0,0 +1,20 @@
+﻿using SubLibrary.CyclopsReferencers;
+using UnityEngine;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class FirePrefabSetter : MonoBehaviour, ICyclopsReferencer
+{
+    [SerializeField] private string firePrefabString;
+    [SerializeField] private PrefabSpawn prefabSpawn;
+
+    private void OnValidate()
+    {
+        if (!prefabSpawn) TryGetComponent(out prefabSpawn);
+    }
+
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        prefabSpawn.prefab = cyclops.transform.Find(firePrefabString).GetComponent<PrefabSpawn>().prefab;
+    }
+}
