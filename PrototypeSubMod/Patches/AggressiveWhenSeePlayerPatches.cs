2789fd2933e82831cebd615230bb96e3a4e24628
Indigocoder1 <caleb@liston.us>
2024-08-16 18:32:28 -0700

    Removed aggression when inside cloak

---
+++ PrototypeSubMod/Patches/AggressiveWhenSeePlayerPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.LightDistortionField;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(AggressiveWhenSeePlayer))]
+internal class AggressiveWhenSeePlayerPatches
+{
+    [HarmonyPatch(nameof(AggressiveWhenSeePlayer.GetAggressionTarget)), HarmonyPostfix]
+    private static void GetAggressionTarget_Postfix(ref GameObject __result)
+    {
+        if (!CloakEffectHandler.Instance) return;
+
+        if(CloakEffectHandler.Instance.IsInsideOvoid(Camera.main.transform.position) && CloakEffectHandler.Instance.effectEnabled)
+        {
+            __result = null;
+        }
+    }
+}
