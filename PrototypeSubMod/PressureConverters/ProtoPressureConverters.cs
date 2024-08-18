98bb0ff7dc87edf079874b33fadff2a4822e94c7
Indigocoder1 <caleb@liston.us>
2024-08-17 18:56:54 -0700

    Added pressure converters script

---
+++ PrototypeSubMod/PressureConverters/ProtoPressureConverters.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.PressureConverters;
+
+internal class ProtoPressureConverters : MonoBehaviour
+{
+    [SerializeField] private CyclopsMotorMode motorMode;
+    [SerializeField] private CrushDamage crushDamage;
+
+    [SerializeField] private float activationDepth;
+    [SerializeField] private float maxDepth;
+    [SerializeField] private AnimationCurve powerMultiplierCurve;
+
+    private float[] originalPowerValues;
+
+    private void Start()
+    {
+        originalPowerValues = motorMode.motorModePowerConsumption;
+    }
+
+    private void FixedUpdate()
+    {
+        float depth = crushDamage.GetDepth();
+
+        if(depth < activationDepth)
+        {
+            motorMode.motorModePowerConsumption = originalPowerValues;
+            return;
+        }
+
+        float normalizedDepth = (depth - activationDepth) / (maxDepth - activationDepth);
+        float multiplier = powerMultiplierCurve.Evaluate(normalizedDepth);
+
+        float[] newPowerValues = new float[originalPowerValues.Length];
+        for (int i = 0; i < newPowerValues.Length; i++)
+        {
+            newPowerValues[i] = originalPowerValues[i] * multiplier;
+        }
+
+        motorMode.motorModePowerConsumption = newPowerValues;
+    }
+}
