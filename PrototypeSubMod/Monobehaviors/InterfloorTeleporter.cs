73dac153d30a08fcc95bcc006c0751a97d3d8786
Indigocoder1 <caleb@liston.us>
2024-08-18 16:50:52 -0700

    Added interfloor teleporter

---
+++ PrototypeSubMod/Monobehaviors/InterfloorTeleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class InterfloorTeleporter : MonoBehaviour
+{
+    [SerializeField] private FMOD_CustomEmitter soundEffect;
+    [SerializeField] private Transform teleportPosition;
+    [SerializeField] private float teleporterCooldown = 1f;
+
+    private bool allowedToTeleport = true;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!allowedToTeleport) return;
+
+        if (col.gameObject != Player.main.gameObject) return;
+
+        Player.main.SetPosition(teleportPosition.position, teleportPosition.rotation);
+        Player.main.mode = Player.Mode.Sitting;
+        FMODUWE.PlayOneShot(soundEffect.asset, teleportPosition.position);
+
+        MainCamera.camera.GetComponent<WarpScreenFXController>().StartWarp();
+
+        StopCoroutine(DelayedCancelWarp());
+        StartCoroutine(DelayedCancelWarp());
+
+        allowedToTeleport = false;
+        Invoke(nameof(ResetAllowedToTeleport), teleporterCooldown);
+    }
+
+    private void ResetAllowedToTeleport()
+    {
+        allowedToTeleport = true;
+    }
+
+    private IEnumerator DelayedCancelWarp()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+
+        Player.main.mode = Player.Mode.Normal;
+        MainCamera.camera.GetComponent<WarpScreenFXController>().StopWarp();
+    }
+}
