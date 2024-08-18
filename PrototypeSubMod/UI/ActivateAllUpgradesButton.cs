b1c271259d24975314b948e437658bb9f9b25a29
Indigocoder1 <caleb@liston.us>
2024-08-17 19:34:50 -0700

    Added ability buttons

---
+++ PrototypeSubMod/UI/ActivateAllUpgradesButton.cs
@@ -0,0 +1,39 @@
+﻿using PrototypeSubMod.Interfaces;
+using UnityEngine;
+
+namespace PrototypeSubMod.UI;
+
+internal class ActivateAllUpgradesButton : MonoBehaviour
+{
+    [SerializeField] private SubRoot subRoot;
+
+    private bool hovering;
+
+    public void OnMouseEnter()
+    {
+        hovering = true;
+    }
+
+    public void OnMouseExit()
+    {
+        hovering = false;
+    }
+
+    private void Update()
+    {
+        if (hovering)
+        {
+            HandReticle main = HandReticle.main;
+            main.SetText(HandReticle.TextType.Hand, "ActivateAllUpgrades_DEBUG", true, GameInput.Button.LeftHand);
+            main.SetText(HandReticle.TextType.HandSubscript, string.Empty, false, GameInput.Button.LeftHand);
+        }
+    }
+
+    public void OnClick()
+    {
+        foreach (var upgrade in subRoot.GetComponentsInChildren<IProtoUpgrade>(true))
+        {
+            upgrade.SetUpgradeActive(true);
+        }
+    }
+}
---
+++ PrototypeSubMod/UI/GenericUIButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.UI;
+
+internal class GenericUIButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onClick;
+    [SerializeField] private string localizationKey;
+
+    private bool hovering;
+
+    public void OnMouseEnter()
+    {
+        hovering = true;
+    }
+
+    public void OnMouseExit()
+    {
+        hovering = false;
+    }
+
+    private void Update()
+    {
+        if (hovering)
+        {
+            HandReticle main = HandReticle.main;
+            main.SetText(HandReticle.TextType.Hand, localizationKey, true, GameInput.Button.LeftHand);
+            main.SetText(HandReticle.TextType.HandSubscript, string.Empty, false, GameInput.Button.LeftHand);
+        }
+    }
+
+    public void OnClick()
+    {
+        onClick?.Invoke();
+    }
+}
