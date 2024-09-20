b71973a48fd5df0b8022d37c11168d8d7d659d7f
Indigocoder1 <caleb@liston.us>
2024-09-19 19:37:07 -0700

    Added repair point manager

---
+++ PrototypeSubMod/Patches/DamageManagerPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.RepairBots;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(CyclopsExternalDamageManager))]
+internal class DamageManagerPatches
+{
+    [HarmonyPatch(nameof(CyclopsExternalDamageManager.CreatePoint)), HarmonyPostfix]
+    private static void CreatePoint_Postfix(CyclopsExternalDamageManager __instance)
+    {
+        if (!__instance.TryGetComponent(out RepairPointManager pointManager)) return;
+
+        pointManager.OnDamagePointsChanged();
+    }
+
+    [HarmonyPatch(nameof(CyclopsExternalDamageManager.RepairPoint)), HarmonyPostfix]
+    private static void RepairPoint_Postfix(CyclopsExternalDamageManager __instance)
+    {
+        if (!__instance.TryGetComponent(out RepairPointManager pointManager)) return;
+
+        pointManager.OnDamagePointsChanged();
+    }
+}
---
+++ PrototypeSubMod/RepairBots/RepairPointManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeSubMod.RepairBots;
+
+internal class RepairPointManager : MonoBehaviour
+{
+    [SerializeField] private Transform damagePointsParent;
+    [SerializeField] private Transform repairPointsParent;
+
+    private Queue<Transform> availableRepairPoints;
+    private List<Transform> assignedPoints;
+
+    public Transform GetRepairPoint()
+    {
+        var point = availableRepairPoints.Dequeue();
+        assignedPoints.Add(point);
+        return point;
+    }
+
+    private void RefreshPointsList()
+    {
+        availableRepairPoints.Clear();
+
+        int index = -1;
+        foreach (Transform child in damagePointsParent)
+        {
+            index++;
+            if (!child.gameObject.activeSelf) continue;
+
+            if (assignedPoints.Contains(child)) continue;
+
+            availableRepairPoints.Enqueue(repairPointsParent.GetChild(index));
+        }
+    }
+
+    public void OnDamagePointsChanged()
+    {
+        RefreshPointsList();
+    }
+}
