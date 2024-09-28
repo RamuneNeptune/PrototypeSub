f12b2aab8848eb28161084819451f8a7a34107b5
Indigocoder1 <caleb@liston.us>
2024-09-28 15:21:48 -0700

    Added ion barrier script

--- PrototypeSubMod/IonBarrier/ProtoIonBarrier.cs
+++ PrototypeSubMod/IonBarrier/ProtoIonBarrier.cs
@@ -1,8 +1,61 @@
 ﻿using PrototypeSubMod.Upgrades;
+using System.Linq;
+using UnityEngine;
 
 namespace PrototypeSubMod.IonBarrier;
 
 internal class ProtoIonBarrier : ProtoUpgrade
 {
-        
+    [SerializeField] private DamageReductor[] damageReductors;
+
+    private float[] multipliers;
+    private DamageType[] damageTypes;
+
+    private DamageReductor[] serializedDamageReductors;
+
+    private void OnValidate()
+    {
+        multipliers = new float[damageReductors.Length];
+        damageTypes = new DamageType[damageReductors.Length];
+
+        for (int i = 0; i < damageReductors.Length; i++)
+        {
+            multipliers[i] = damageReductors[i].reductionMultiplier;
+            damageTypes[i] = damageReductors[i].type;
+        }
+    }
+
+    private void Start()
+    {
+        serializedDamageReductors = new DamageReductor[multipliers.Length];
+        for (int i = 0; i < multipliers.Length; i++)
+        {
+            serializedDamageReductors[i] = new(multipliers[i], damageTypes[i]);
+        }
+    }
+
+    public float GetReductionForType(DamageType type)
+    {
+        DamageReductor reductor = serializedDamageReductors.FirstOrDefault(r => r.type == type);
+
+        if (reductor == null)
+        {
+            return 1;
+        }
+
+        return reductor.reductionMultiplier;
+    }
+}
+
+[System.Serializable]
+internal class DamageReductor
+{
+    public float reductionMultiplier;
+    public DamageType type;
+
+    public DamageReductor(float reductionMultiplier, DamageType type)
+    {
+        this.reductionMultiplier = reductionMultiplier;
+        this.type = type;
+    }
 }
---
+++ PrototypeSubMod/Patches/DamageSystemPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.IonBarrier;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(DamageSystem))]
+internal class DamageSystemPatches
+{
+    [HarmonyPatch(nameof(DamageSystem.CalculateDamage)), HarmonyPrefix]
+    private static void CalculateDamage_Prefix(ref float damage, DamageType type, GameObject target)
+    {
+        var ionBarrier = target.GetComponentInChildren<ProtoIonBarrier>(true);
+        if (ionBarrier == null) return;
+
+        if (!ionBarrier.GetUpgradeEnabled() || !ionBarrier.GetUpgradeInstalled()) return;
+
+        damage *= ionBarrier.GetReductionForType(type);
+    }
+}
