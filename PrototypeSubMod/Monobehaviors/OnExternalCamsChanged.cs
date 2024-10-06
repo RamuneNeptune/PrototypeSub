2cd3b71490c95d00f21878b95fc9c3da743c1fed
Indigocoder1 <caleb@liston.us>
2024-10-05 18:25:17 -0700

    Added camera model disabler

---
+++ PrototypeSubMod/Monobehaviors/OnExternalCamsChanged.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+[RequireComponent(typeof(CyclopsExternalCams))]
+internal class OnExternalCamsChanged : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onCamsEnabled;
+    [SerializeField] private UnityEvent onCamsDisabled;
+
+    private CyclopsExternalCams externalCams;
+    private bool enabledLastFrame;
+
+    private void Start()
+    {
+        externalCams = GetComponent<CyclopsExternalCams>();
+    }
+
+    private void LateUpdate()
+    {
+        if (externalCams.active != enabledLastFrame)
+        {
+            if (externalCams.active) onCamsEnabled?.Invoke();
+            if (!externalCams.active) onCamsDisabled?.Invoke();
+        }
+
+        enabledLastFrame = externalCams.active;
+    }
+}
