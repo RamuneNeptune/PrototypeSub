c36ffae6f4cf13482b18b51486cd324054a34e16
Indigocoder1 <caleb@liston.us>
2024-10-05 19:08:55 -0700

    Added custom camera icon

---
+++ PrototypeSubMod/Monobehaviors/CyclopsCameraIconReplacer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class CyclopsCameraIconReplacer : MonoBehaviour
+{
+    private const string BG_PATH = "Content/CameraCyclops/DirectionIndicator/DirectionCyclops";
+
+    [SerializeField] private Sprite replacementSprite;
+
+    private Sprite originalSprite;
+    private Image backgroundImage;
+
+    private void Start()
+    {
+        backgroundImage = uGUI_CameraCyclops.main.transform.Find(BG_PATH).GetComponent<Image>();
+        originalSprite = backgroundImage.sprite;
+    }
+
+    public void OnCamerasEntered()
+    {
+        backgroundImage.sprite = replacementSprite;
+    }
+
+    public void OnCamerasExited() 
+    {
+        backgroundImage.sprite = originalSprite;
+    }
+}
