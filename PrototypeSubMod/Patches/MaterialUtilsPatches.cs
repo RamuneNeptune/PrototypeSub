5e1620ed52b09819e1ef5e903619aef4f8a47ad7
Indigocoder1 <caleb@liston.us>
2024-10-12 11:13:54 -0700

    Added material utils patch

---
+++ PrototypeSubMod/Patches/MaterialUtilsPatches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(MaterialUtils))]
+internal class MaterialUtilsPatches
+{
+    [HarmonyPatch(nameof(MaterialUtils.ApplyUBERShader)), HarmonyPrefix]
+    private static void ApplyUBERShader_Prefix(Material material, ref float __state)
+    {
+        __state = material.GetFloat("_GlossMapScale");
+    }
+
+    [HarmonyPatch(nameof(MaterialUtils.ApplyUBERShader)), HarmonyPostfix]
+    private static void ApplyUBERShader_Postfix(Material material, float __state)
+    {
+        material.SetFloat("_Smoothness", __state * 8f);
+    }
+}
