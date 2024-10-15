cbf946c30e753a4b6dc2e7b00f1b414d1d0cf22b
Indigocoder1 <caleb@liston.us>
2024-10-14 20:54:49 -0700

    Consumption button fixes

--- PrototypeSubMod/PowerSystem/AbilityConsumptionButton.cs
+++ PrototypeSubMod/PowerSystem/AbilityConsumptionButton.cs
@@ -4,25 +4,34 @@ using UnityEngine.UI;
 
 namespace PrototypeSubMod.PowerSystem;
 
-internal class AbilityConsumptionButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+internal class AbilityConsumptionButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private Button button;
+    [SerializeField] private Image buttonImage;
     [SerializeField] private Image progressBar;
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite hoverSprite;
+    [SerializeField] private Sprite pressedSprite;
+    [SerializeField] private Sprite disabledSprite;
     [SerializeField] private Animator animator;
     [SerializeField] private float confirmTime; 
 
     private float currentConfirmTime;
     private bool clicking;
+    private bool hovering;
     private bool consumed;
+    private bool interactable;
 
     private void Start()
     {
         animator.speed = 1 / confirmTime;
+        progressBar.fillAmount = 0;
     }
 
     private void Update()
     {
-        if (!clicking || !button.interactable)
+        UpdateButtonSprite();
+
+        if (!clicking || !interactable)
         {
             currentConfirmTime = 0;
             consumed = false;
@@ -36,30 +45,31 @@ internal class AbilityConsumptionButton : MonoBehaviour, IPointerDownHandler, IP
             consumed = true;
             progressBar.fillAmount = 0;
             ProtoPowerAbilitySystem.Instance.ConsumeItem();
+            animator.SetBool("Charging", false);
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         clicking = true;
-        animator.SetBool("Charging", true);
+
+        if (interactable) animator.SetBool("Charging", true);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         clicking = false;
-        animator.SetBool("Charging", false);
     }
 
     private void OnEnable()
     {
         if (ProtoPowerAbilitySystem.Instance == null)
         {
-            button.interactable = false;
+            interactable = false;
             return;
         }
 
-        button.interactable = ProtoPowerAbilitySystem.Instance.HasItem();
+        interactable = ProtoPowerAbilitySystem.Instance.HasItem();
         ProtoPowerAbilitySystem.Instance.onEquip += EnableButton;
         ProtoPowerAbilitySystem.Instance.onUnequip += DisableButton;
     }
@@ -74,15 +84,35 @@ internal class AbilityConsumptionButton : MonoBehaviour, IPointerDownHandler, IP
 
     private void EnableButton()
     {
-        button.interactable = true;
-        button.image.color = Color.white;
+        interactable = true;
+        buttonImage.color = Color.white;
         progressBar.fillAmount = 0;
     }
 
     private void DisableButton()
     {
-        button.interactable = false;
-        button.image.color = new Color(0.75f, 0.75f, 0.75f);
+        interactable = false;
+        buttonImage.color = new Color(0.75f, 0.75f, 0.75f);
         progressBar.fillAmount = 0;
+    }
+
+    private void UpdateButtonSprite()
+    {
+        Sprite sprite = normalSprite;
+        if (hovering) sprite = hoverSprite;
+        if (clicking) sprite = pressedSprite;
+        if (!interactable) sprite = disabledSprite;
+
+        buttonImage.sprite = sprite;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovering = false;
     }
 }
