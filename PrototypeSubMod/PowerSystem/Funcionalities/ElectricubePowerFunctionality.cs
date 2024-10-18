f8b9ae42f38c5d633a9cbdd1eb9b8e7d3bcf83b7
Indigocoder1 <caleb@liston.us>
2024-10-17 18:56:08 -0700

    Added teleporter fx color manager

--- PrototypeSubMod/PowerSystem/Funcionalities/ElectricubePowerFunctionality.cs
+++ PrototypeSubMod/PowerSystem/Funcionalities/ElectricubePowerFunctionality.cs
@@ -8,20 +8,24 @@ namespace PrototypeSubMod.PowerSystem.Funcionalities;
 internal class ElectricubePowerFunctionality : PowerSourceFunctionality
 {
     private static readonly Color ElectricubeLightColor = new Color(219 / 255f, 191 / 255f, 255 / 255f);
+    private static readonly Color ElectricubeTeleporterColor = new Color(219 / 255f, 191 / 255f, 255 / 255f);
 
     private LightColorHandler colorHandler;
     private ProtoMotorHandler motorHandler;
+    private TeleporterFXColorManager colorManager;
 
     public override void OnAbilityActivated()
     {
         var root = GetComponentInParent<SubRoot>();
         colorHandler = root.GetComponentInChildren<LightColorHandler>();
         motorHandler = root.GetComponentInChildren<ProtoMotorHandler>();
+        colorManager = root.GetComponentInChildren<TeleporterFXColorManager>();
 
         colorHandler.SetTempColor(ElectricubeLightColor);
         motorHandler.SetPowerEfficiencyMultiplier(1.2f);
         ProtoTeleporterManager.Instance.SetPowerMultiplier(0.85f);
-        TeleporterOverride.SetTempTeleporterColor(ElectricubeLightColor);
+        TeleporterOverride.SetTempTeleporterColor(ElectricubeTeleporterColor);
+        colorManager.SetTempColor(ElectricubeTeleporterColor);
     }
 
     protected override void OnAbilityStopped()
@@ -30,5 +34,6 @@ internal class ElectricubePowerFunctionality : PowerSourceFunctionality
         motorHandler.SetPowerEfficiencyMultiplier(1f);
         ProtoTeleporterManager.Instance.SetPowerMultiplier(1f);
         TeleporterOverride.ResetTeleporterColor();
+        colorManager.ResetColor();
     }
 }
---
+++ PrototypeSubMod/Teleporter/TeleporterFXColorManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeSubMod.Teleporter;
+
+internal class TeleporterFXColorManager : MonoBehaviour
+{
+    [SerializeField] private Transform fxParent;
+
+    private Dictionary<Renderer, Color> originalColors;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitUntil(() => fxParent.childCount > 0);
+
+        foreach (var rend in fxParent.GetComponentsInChildren<Renderer>())
+        {
+            originalColors.Add(rend, rend.material.color);
+        }
+    }
+
+    public void SetTempColor(Color color)
+    {
+        foreach (var rend in originalColors.Keys)
+        {
+            rend.material.color = color;
+        }
+    }
+
+    public void ResetColor()
+    {
+        foreach (var kvp in originalColors)
+        {
+            kvp.Key.material.color = kvp.Value;
+        }
+    }
+}
