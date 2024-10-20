ab0464301d816c655f0ac80b4aad90f90986057f
Indigocoder1 <caleb@liston.us>
2024-10-19 17:39:51 -0700

    Anim changes & added depot bay proxy

---
+++ PrototypeSubMod/PowerSystem/PowerAbilitySystemProxy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.PowerSystem;
+
+[RequireComponent(typeof(Collider))]
+internal class PowerAbilitySystemProxy : MonoBehaviour
+{
+    [SerializeField] private ProtoPowerAbilitySystem abilitySystem;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!col.gameObject.Equals(Player.main.gameObject)) return;
+
+        abilitySystem.OnEnterProxy();
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (!col.gameObject.Equals(Player.main.gameObject)) return;
+
+        abilitySystem.OnExitProxy();
+    }
+}
