5e815451f960de73f99a4da737c0469ee454890d
Indigocoder1 <caleb@liston.us>
2024-10-20 19:48:57 -0700

    Fixed player lever anims not working

---
+++ PrototypeSubMod/Monobehaviors/FollowObjectPos.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class FollowObjectPos : MonoBehaviour
+{
+    [SerializeField] private Transform followObj;
+    [SerializeField] private Vector3 localOffset;
+
+    private void LateUpdate()
+    {
+        transform.localPosition = transform.parent.InverseTransformPoint(followObj.position) + localOffset;
+    }
+}
