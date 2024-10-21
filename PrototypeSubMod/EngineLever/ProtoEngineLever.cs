3a84c70e0f9ae68c2f1b073f675e31a07d8bd462
Indigocoder1 <caleb@liston.us>
2024-10-20 19:22:21 -0700

    New engine toggle terminal & animations

---
+++ PrototypeSubMod/EngineLever/ProtoEngineLever.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.EngineLever;
+
+internal class ProtoEngineLever : CinematicModeTriggerBase
+{
+    [SerializeField] private Animator leverAnimator;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private CyclopsMotorMode motorMode;
+    [SerializeField] private Transform leftIKTarget;
+    [SerializeField] private Transform rightIKTarget;
+
+    private void Start()
+    {
+        cinematicController.animator = Player.main.playerAnimator;
+    }
+
+    public override void OnHandHover(GUIHand hand)
+    {
+        string key = motorMode.engineOn ? "ProtoEngineLeverDisable" : "ProtoEngineLeverEnable";
+        var main = HandReticle.main;
+        main.SetText(HandReticle.TextType.Hand, key, true, GameInput.Button.LeftHand);
+        main.SetText(HandReticle.TextType.HandSubscript, string.Empty, false, GameInput.Button.None);
+        main.SetIcon(HandReticle.IconType.Hand, 1f);
+    }
+
+    public override void OnStartCinematicMode()
+    {
+        base.OnStartCinematicMode();
+        Player.main.armsController.SetWorldIKTarget(leftIKTarget, rightIKTarget);
+
+        bool nextState = !leverAnimator.GetBool("LeverEnabled");
+        leverAnimator.SetBool("LeverEnabled", nextState);
+        playerAnimator.SetTrigger(nextState ? "LeverDown" : "LeverUp");
+    }
+    
+    public void OnCinematicEnd()
+    {
+        Player.main.armsController.SetWorldIKTarget(null, null);
+    }
+}
