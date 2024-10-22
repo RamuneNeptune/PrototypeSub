d32c0e4e0a4c281e021c42fefc558aa6143225e0
Indigocoder1 <caleb@liston.us>
2024-10-21 20:39:58 -0700

    Anims actually look kinda good now

---
+++ PrototypeSubMod/Monobehaviors/AnimatorEventRelay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+// Eld don't kill me for this one please
+internal class AnimatorEventRelay : MonoBehaviour
+{
+    public UnityEvent onEventTriggered;
+
+    public void TriggerEvent()
+    {
+        onEventTriggered?.Invoke();
+    }
+}
--- PrototypeSubMod/Monobehaviors/FollowObjectPos.cs
+++ PrototypeSubMod/Monobehaviors/FollowObjectPos.cs
@@ -5,10 +5,9 @@ namespace PrototypeSubMod.Monobehaviors;
 internal class FollowObjectPos : MonoBehaviour
 {
     [SerializeField] private Transform followObj;
-    [SerializeField] private Vector3 localOffset;
 
     private void LateUpdate()
     {
-        transform.position = followObj.position + transform.parent.TransformPoint(localOffset);
+        transform.position = followObj.position;
     }
 }
