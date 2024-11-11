d8c41763a98c4e03277fab23d445fe7f0c98df37
Indigocoder1 <caleb@liston.us>
2024-11-10 18:30:48 -0800

    Version increment

--- PrototypeSubMod/Plugin.cs
+++ PrototypeSubMod/Plugin.cs
@@ -32,7 +32,7 @@ namespace PrototypeSubMod
     {
         private const string GUID = "com.teamproto.prototypesub";
         private const string pluginName = "Prototype Sub";
-        private const string versionString = "0.0.6.0";
+        private const string versionString = "0.0.6.1";
 
         public new static ManualLogSource Logger { get; private set; }
 
