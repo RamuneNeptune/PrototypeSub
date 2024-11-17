f8fff7294962079e694e5ce537774c91778d95f6
Indigocoder1 <caleb@liston.us>
2024-11-17 13:54:15 -0800

    Added multipurpose alien terminal

---
+++ PrototypeSubMod/Facilities/MultipurposeAlienTerminal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UWE;
+
+namespace PrototypeSubMod.Facilities;
+
+internal class MultipurposeAlienTerminal : MonoBehaviour
+{
+    public event Action onTerminalInteracted;
+
+    public string primaryTooltip = "GenericConsole";
+    public string secondaryTooltip = "Tooltip_GenericConsole";
+
+    private IEnumerator Start()
+    {
+        var prefabRequest = PrefabDatabase.GetPrefabAsync("d200d747-b802-43f4-80b1-5c3d2155fbcd");
+
+        yield return prefabRequest;
+
+        GameObject prefab = null;
+        if (!prefabRequest.TryGetPrefab(out prefab)) throw new Exception("Error retrieving alien terminal prefab");
+
+        var instance = Instantiate(prefab, transform, false);
+        instance.transform.localPosition = Vector3.zero;
+        instance.transform.localRotation = Quaternion.identity;
+        instance.transform.localScale = Vector3.one;
+
+        var storyTarget = instance.GetComponentInChildren<StoryHandTarget>();
+        var protoTarget = storyTarget.gameObject.EnsureComponent<ProtoTerminalHandTarget>();
+
+        protoTarget.destroyGameObject = storyTarget.destroyGameObject;
+        protoTarget.informGameObject = storyTarget.informGameObject;
+        protoTarget.primaryTooltip = primaryTooltip;
+        protoTarget.secondaryTooltip = secondaryTooltip;
+    }
+}
---
+++ PrototypeSubMod/Facilities/ProtoTerminalHandTarget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Facilities;
+
+internal class ProtoTerminalHandTarget : MonoBehaviour
+{
+    public void OnHandHover(GUIHand hand)
+    {
+        HandReticle.main.SetText(HandReticle.TextType.Hand, primaryTooltip, true, GameInput.Button.LeftHand);
+        HandReticle.main.SetText(HandReticle.TextType.HandSubscript, secondaryTooltip, true, GameInput.Button.None);
+        HandReticle.main.SetIcon(HandReticle.IconType.Hand, 1f);
+    }
+
+    public void OnHandClick(GUIHand hand)
+    {
+        if (informGameObject)
+        {
+            informGameObject.SendMessage("OnStoryHandTarget", SendMessageOptions.DontRequireReceiver);
+        }
+        Destroy(destroyGameObject);
+    }
+
+    public string primaryTooltip;
+    public string secondaryTooltip;
+
+    public GameObject informGameObject;
+    public GameObject destroyGameObject;
+}
