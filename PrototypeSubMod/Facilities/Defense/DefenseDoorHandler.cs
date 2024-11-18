25121fc65000600db09b4109ca4359ce320c634c
Indigocoder1 <caleb@liston.us>
2024-11-17 17:12:38 -0800

    Added **Big door**

---
+++ PrototypeSubMod/Facilities/Defense/DefenseDoorHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Defense;
+
+internal class DefenseDoorHandler : MonoBehaviour
+{
+    [SerializeField] private FMODAsset doorOpenSFX;
+    [SerializeField] private FMODAsset doorSwooshSFX;
+    [SerializeField] private Transform openSFXPos;
+    [SerializeField] private Animator animator;
+
+    public void OpenDoor()
+    {
+        FMODUWE.PlayOneShot(doorOpenSFX, openSFXPos.position);
+        FMODUWE.PlayOneShot(doorSwooshSFX, openSFXPos.position);
+        animator.SetTrigger("OpenDoor");
+    }
+}
