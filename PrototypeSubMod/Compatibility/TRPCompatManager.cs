7ee127e9e8274abf12bd82710f7f11169a1a3248
Indigocoder1 <caleb@liston.us>
2024-11-23 13:55:34 -0800

    Added compat manager

---
+++ PrototypeSubMod/Compatibility/TRPCompatManager.cs
@@ -0,0 +1,21 @@
+﻿namespace PrototypeSubMod.Compatibility;
+
+internal static class TRPCompatManager
+{
+    public static bool TRPInstalled
+    {
+        get
+        {
+            if (!_trpInitted)
+            {
+                _trpInstalled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.lee23.theredplague");
+                _trpInitted = true;
+            }
+
+            return _trpInstalled;
+        }
+    }
+
+    private static bool _trpInitted;
+    private static bool _trpInstalled;
+}
