5e0bbd2306c3c0b499f91ebc66be8d7a7e681ce4
Indigocoder1 <caleb@liston.us>
2024-11-23 14:04:17 -0800

    Added island manager & fall teleporters

---
+++ PrototypeSubMod/TRPCompat/DefenseFacilityTRPManager.cs
@@ -0,0 +1,16 @@
+﻿using PrototypeSubMod.Compatibility;
+using UnityEngine;
+
+namespace PrototypeSubMod.TRPCompat;
+
+internal class DefenseFacilityTRPManager : MonoBehaviour
+{
+    [SerializeField] private GameObject trpIsland;
+    [SerializeField] private Collider safetyTeleporterCutout;
+
+    private void Start()
+    {
+        trpIsland.SetActive(TRPCompatManager.TRPInstalled);
+        safetyTeleporterCutout.enabled = !TRPCompatManager.TRPInstalled;
+    }
+}
