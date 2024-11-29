58ea60ffc2621a426096f7eeb40a31750c626380
Indigocoder1 <caleb@liston.us>
2024-11-29 15:42:05 -0800

    Added ApplyCyclopsGlass script

---
+++ PrototypeSubMod/Monobehaviors/ApplyCyclopsGlass.cs
@@ -0,0 +1,22 @@
+﻿using SubLibrary.CyclopsReferencers;
+using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.Monobehaviors;
+
+internal class ApplyCyclopsGlass : MonoBehaviour, ICyclopsReferencer
+{
+    [SerializeField] private Renderer renderer;
+
+    private void OnValidate()
+    {
+        if (!renderer) TryGetComponent(out renderer);
+    }
+
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        var glassObj = cyclops.transform.Find("CyclopsMeshStatic/undamaged/cyclops_LOD0/Cyclops_submarine_exterior_glass");
+        var rend = glassObj.GetComponent<Renderer>();
+        renderer.material = new(rend.material);
+    }
+}
