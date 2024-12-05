a9931296ce143dd68c9d02cce6085b01b154b968
Indigocoder1 <caleb@liston.us>
2024-12-04 18:58:13 -0800

    Added watergate block

--- PrototypeSubMod/Plugin.cs
+++ PrototypeSubMod/Plugin.cs
@@ -251,6 +251,7 @@ namespace PrototypeSubMod
         {
             ConsoleCommandsHandler.AddGotoTeleportPosition("interceptorfacility", new Vector3(547, -709, 955));
             ConsoleCommandsHandler.AddGotoTeleportPosition("defensefacility", new Vector3(689, -483, -1404f));
+            ConsoleCommandsHandler.AddGotoTeleportPosition("enginefacility", new Vector3(306, -1156, 131f));
         }
     }
 }
