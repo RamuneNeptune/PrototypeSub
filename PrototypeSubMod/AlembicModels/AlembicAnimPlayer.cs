5a259a64e9dc0319738b0f0e67c6bcaa885bb82f
Indigocoder1 <caleb@liston.us>
2024-12-07 13:45:58 -0800

    Got alembic stuff auto-fixing

---
+++ PrototypeSubMod/AlembicModels/AlembicAnimPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
+
+namespace PrototypeSubMod.AlembicModels;
+
+internal class AlembicAnimPlayer : MonoBehaviour
+{
+    [SerializeField] private AlembicStreamPlayer alemPlayer;
+    [SerializeField] private float playbackSpeed = 1;
+
+    private void Update()
+    {
+        alemPlayer.CurrentTime += playbackSpeed * Time.deltaTime;
+        alemPlayer.CurrentTime = alemPlayer.CurrentTime % alemPlayer.EndTime;
+    }
+}
