cc9750a202d8293eb0ad312e36c145b65ae514de
Indigocoder1 <caleb@liston.us>
2024-12-10 17:33:00 -0800

    Added colliders to facility tunnel

---
+++ PrototypeSubAssets/Assets/Scripts/RemoveMeshes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RemoveMeshes : MonoBehaviour
+{
+    [SerializeField] private bool remove;
+
+    private void Update()
+    {
+        if (!remove) return;
+        remove = false;
+        var rends = GetComponentsInChildren<MeshRenderer>(true);
+        for (int i = rends.Length - 1; i >= 0; i--)
+        {
+            var filter = rends[i].GetComponent<MeshFilter>();
+            DestroyImmediate(filter);
+            DestroyImmediate(rends[i]);
+        }
+    }
+}
