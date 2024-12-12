97295059fcb45613cb6ade8db6db48be0736c4ed
Indigocoder1 <caleb@liston.us>
2024-12-11 19:51:36 -0800

    Added collider areas to tunnel

---
+++ PrototypeSubMod/MiscMonobehaviors/ColliderArea.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+internal class ColliderArea : MonoBehaviour
+{
+    [SerializeField] private GameObject[] colliders;
+
+    private void Start()
+    {
+        foreach (var obj in colliders)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col != Player.mainCollider) return;
+
+        foreach (var obj in colliders)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col != Player.mainCollider) return;
+
+        foreach (var obj in colliders)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
