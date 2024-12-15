023fe86388bf968bb9f0f4e80e02ac43d955d5bc
Indigocoder1 <caleb@liston.us>
2024-12-14 16:14:07 -0800

    Added fall-enabler area above moonpool door

---
+++ PrototypeSubMod/MiscMonobehaviors/SetPickupsToFall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+internal class SetPickupsToFall : MonoBehaviour
+{
+    [SerializeField] private Transform root;
+    [SerializeField] private Vector3 localOffset;
+    [SerializeField] private Vector3 halfExtents;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitUntil(LargeWorldStreamer.main.IsWorldSettled);
+
+        int numItems = UWE.Utils.OverlapBoxIntoSharedBuffer(transform.position + localOffset, halfExtents, root.rotation);
+        for (int i = 0; i < numItems; i++)
+        {
+            Collider col = UWE.Utils.sharedColliderBuffer[i];
+            var rb = col.GetComponentInParent<Rigidbody>();
+            var pickupable = col.GetComponentInParent<Pickupable>();
+
+            if (rb == null || pickupable == null) continue;
+
+            if (rb.GetComponentInParent<SubRoot>() != null) continue;
+
+            UWE.Utils.SetIsKinematicAndUpdateInterpolation(rb, false);
+            pickupable.isKinematic = PickupableKinematicState.NonKinematic;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(localOffset, halfExtents * 2);
+    }
+}
