e2dbceec1d2c96306eaebb670e8abf5b55044bc9
Indigocoder1 <caleb@liston.us>
2024-12-19 14:48:32 -0800

    Added generic player trigger class

---
+++ PrototypeSubMod/MiscMonobehaviors/GenericPlayerTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+[RequireComponent(typeof(Collider))]
+internal class GenericPlayerTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onTriggerEnter;
+    [SerializeField] private UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != Player.mainCollider) return;
+
+        onTriggerEnter?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != Player.mainCollider) return;
+
+        onTriggerExit?.Invoke();
+    }
+}
