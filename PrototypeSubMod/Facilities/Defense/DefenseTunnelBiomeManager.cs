027c288608d0526e0c990e1535b73def8d8e1cec
Indigocoder1 <caleb@liston.us>
2024-12-19 14:57:53 -0800

    Added defense tunnel biome reverser

---
+++ PrototypeSubMod/Facilities/Defense/DefenseTunnelBiomeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Defense;
+
+internal class DefenseTunnelBiomeManager : MonoBehaviour
+{
+    [SerializeField] private AtmosphereVolume[] atmosphereVolumes;
+    [SerializeField] private string baseBiomeName;
+
+    public void SetBiomesNormal()
+    {
+        for (int i = 0; i < atmosphereVolumes.Length; i++)
+        {
+            atmosphereVolumes[i].overrideBiome = $"{baseBiomeName}{i + 1}";
+        }
+    }
+
+    public void SetBiomesReversed()
+    {
+        for (int i = 0; i < atmosphereVolumes.Length; i++)
+        {
+            atmosphereVolumes[atmosphereVolumes.Length - 1 - i].overrideBiome = $"{baseBiomeName}{i + 1}";
+        }
+    }
+}
