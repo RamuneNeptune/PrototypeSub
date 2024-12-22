9ceec3f41478ad52b03f8314f0777dd8abeae94c
Indigocoder1 <caleb@liston.us>
2024-12-22 12:22:22 -0800

    Added localization to build anim screen

--- PrototypeSubMod/SubTerminal/uGUI_BuildAnimScreen.cs
+++ PrototypeSubMod/SubTerminal/uGUI_BuildAnimScreen.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace PrototypeSubMod.SubTerminal;
 
 internal class uGUI_BuildAnimScreen : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI displayText;
     [SerializeField] private Image progressBar;
+    [SerializeField] private string chargingLocalizationKey;
+    [SerializeField] private string buildingLocalizationKey;
 
     private float duration;
     private float currentProgress;
@@ -17,11 +21,18 @@ internal class uGUI_BuildAnimScreen : MonoBehaviour
         this.duration = duration;
         currentProgress = 0;
         startAnim = true;
+
+        displayText.text = Language.main.Get(buildingLocalizationKey);
     }
 
     public void StartPreWarm(float duration)
     {
+        displayText.text = Language.main.Get(chargingLocalizationKey);
 
+        progressBar.fillAmount = 0;
+        this.duration = duration;
+        currentProgress = 0;
+        startAnim = true;
     }
 
     private void Update()
