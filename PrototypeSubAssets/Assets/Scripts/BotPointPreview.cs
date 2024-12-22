3c2192250267e2269c736220a824ad183ed1f714
Indigocoder1 <caleb@liston.us>
2024-12-22 15:02:15 -0800

    Added beam point preview script

---
+++ PrototypeSubAssets/Assets/Scripts/BotPointPreview.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+public class BotPointPreview : MonoBehaviour
+{
+    [SerializeField] private bool showPreview;
+
+    private void OnDrawGizmos()
+    {
+        if (!showPreview) return;
+
+        Gizmos.color = Color.cyan;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var childA = transform.GetChild(i);
+            Gizmos.DrawCube(childA.position, Vector3.one * 0.25f);
+        }
+    }
+}
