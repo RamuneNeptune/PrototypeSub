406cff1689b3d7e3736662cbd40617bda9d74775
Indigocoder1 <caleb@liston.us>
2024-12-22 14:57:02 -0800

    Added bot path preview script

---
+++ PrototypeSubAssets/Assets/Scripts/BotPathPreview.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+public class BotPathPreview : MonoBehaviour
+{
+    [SerializeField] private bool showPreview;
+
+    private void OnDrawGizmos()
+    {
+        if (!showPreview) return;
+
+        if (transform.childCount <= 1) return;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Gizmos.color = Color.blue;
+
+            var childA = transform.GetChild(i);
+            var childB = transform.GetChild((i + 1) % transform.childCount);
+            Gizmos.DrawLine(childA.position, childB.position);
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawCube(childA.position, Vector3.one * 0.25f);
+        }
+    }
+}
