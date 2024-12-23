bb25abdbbc5add0974ea40a40033de07d5f2349d
Indigocoder1 <caleb@liston.us>
2024-12-23 14:18:02 -0800

    Made holo display color represent sub health

--- PrototypeSubMod/StatsTerminal/ProtoStatsTerminal.cs
+++ PrototypeSubMod/StatsTerminal/ProtoStatsTerminal.cs
@@ -5,6 +5,18 @@ namespace PrototypeSubMod.StatsTerminal;
 internal class ProtoStatsTerminal : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private LiveMixin liveMixin;
+    [SerializeField] private GameObject modelsParent;
+    [SerializeField] private Gradient holoColorOverHealth;
+    [SerializeField] private float colorTransitionSpeed;
+
+    private Renderer[] renderers;
+    private Color currentColor;
+
+    private void Start()
+    {
+        renderers = modelsParent.GetComponentsInChildren<Renderer>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,5 +30,21 @@ internal class ProtoStatsTerminal : MonoBehaviour
         if (!other.gameObject.Equals(Player.main.gameObject)) return;
 
         animator.SetBool("TerminalOpen", false);
+    }
+
+    private void Update()
+    {
+        currentColor = renderers[0].material.color;
+        var targetColor = Color.Lerp(currentColor, holoColorOverHealth.Evaluate(liveMixin.health / liveMixin.maxHealth), Time.deltaTime * colorTransitionSpeed);
+
+        foreach (var rend in renderers)
+        {
+            var mats = rend.materials;
+            for (int i = 0; i < mats.Length; i++)
+            {
+                mats[i].color = targetColor;
+            }
+            rend.materials = mats;
+        }
     }
 }
