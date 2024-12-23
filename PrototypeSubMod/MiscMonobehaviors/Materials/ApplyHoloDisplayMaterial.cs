7fb4de5bce088890b0130ecab02c22f68054332b
Indigocoder1 <caleb@liston.us>
2024-12-23 10:50:36 -0800

    Added apply holo display material script

---
+++ PrototypeSubMod/MiscMonobehaviors/Materials/ApplyHoloDisplayMaterial.cs
@@ -0,0 +1,41 @@
+﻿using SubLibrary.CyclopsReferencers;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.Materials;
+
+internal class ApplyHoloDisplayMaterial : MonoBehaviour, ICyclopsReferencer
+{
+    [SerializeField] private ApplyMode applyMode;
+    [SerializeField] private Color holoColor = new Color(0.4559f, 0.9082f, 1f, 1f);
+
+    [Header("Single Rend Mode")]
+    [SerializeField] private Renderer singleRenderer;
+
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        var borderObj = cyclops.transform.Find("HolographicDisplay/HolographicDisplayVisuals/CyclopsMini_Mid/border");
+        var copyRend = borderObj.GetComponent<Renderer>();
+
+        var newMaterial = new Material(copyRend.material);
+        newMaterial.color = holoColor;
+
+        switch (applyMode)
+        {
+            case ApplyMode.SingleRend:
+                singleRenderer.material = newMaterial;
+                break;
+            case ApplyMode.AllChildren:
+                foreach (var rend in GetComponentsInChildren<Renderer>())
+                {
+                    rend.material = newMaterial;
+                }
+                break;
+        }
+    }
+
+    private enum ApplyMode
+    {
+        SingleRend,
+        AllChildren
+    }
+}
