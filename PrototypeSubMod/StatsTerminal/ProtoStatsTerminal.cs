2e7cac7247630e53c6f245d1dd6ba153d0b9489f
Indigocoder1 <caleb@liston.us>
2024-12-23 08:52:20 -0800

    Added ProtoStatsTerminal

---
+++ PrototypeSubMod/StatsTerminal/ProtoStatsTerminal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.StatsTerminal;
+
+internal class ProtoStatsTerminal : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != Player.mainCollider) return;
+
+        animator.SetBool("TerminalOpen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != Player.mainCollider) return;
+
+        animator.SetBool("TerminalOpen", true);
+    }
+}
