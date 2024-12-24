724b6a6324553226a0b847946b43a06e852ce094
Indigocoder1 <caleb@liston.us>
2024-12-23 18:28:48 -0800

    Fixed attach point orientations

--- PrototypeSubAssets/Assets/Scripts/LarvaAttachPointPreview.cs
+++ PrototypeSubAssets/Assets/Scripts/LarvaAttachPointPreview.cs
@@ -3,17 +3,31 @@
 public class LarvaAttachPointPreview : MonoBehaviour
 {
     public Mesh cubeMesh;
+    public bool flipOrientations;
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-
         if (cubeMesh == null) return;
 
         for (int i = 0; i < transform.childCount; i++)
         {
             var child = transform.GetChild(i);
+
+            if (flipOrientations)
+            {
+                child.Rotate(new Vector3(0, 180, 0), Space.Self);
+            }
+
+            Gizmos.color = Color.red;
             Gizmos.DrawMesh(cubeMesh, 0, child.position, child.rotation, new Vector3(child.localScale.x, child.localScale.y, child.localScale.z * 0.01f));
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(child.position, -child.forward);
+        }
+
+        if (flipOrientations)
+        {
+            flipOrientations = false;
         }
     }
 }
