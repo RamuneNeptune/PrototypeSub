43dfc4062f331ee7a0a3e5e3ca01559c8ad25a42
Indigocoder1 <caleb@liston.us>
2024-12-23 20:13:00 -0800

    Made warning pings work when spawning closed

--- PrototypeSubMod/StatsTerminal/ProtoWarningPing.cs
+++ PrototypeSubMod/StatsTerminal/ProtoWarningPing.cs
@@ -17,34 +17,37 @@ internal class ProtoWarningPing : MonoBehaviour
 
     private void Start()
     {
-        if (warningType == CyclopsHolographicHUD_WarningPings.WarningTypes.Damage)
+        if (warningType != CyclopsHolographicHUD_WarningPings.WarningTypes.Damage) return;
+        if (damageText == null) return;
+        if (labelDot == null) return;
+        if (lineRenderer == null) return;
+
+        var textRect = damageText.transform.GetChild(0).GetComponent<RectTransform>();
+        textRect.localScale = Vector3.one * 23.33f;
+        float xPos = -1167.760f * textRect.localPosition.x;
+        textRect.localPosition = new Vector3(xPos, 5.870f, -189.170f);
+
+        var labelRect = warningPing.GetComponent<RectTransform>();
+        labelRect.sizeDelta = new Vector2(1600, 1584);
+
+        Vector3 parentOffset = (parent.InverseTransformPoint(transform.localPosition) - parent.position).normalized;
+        Vector3 dotPos = parent.TransformPoint(transform.position + parentOffset * 0.35f);
+        labelDot.transform.localPosition = dotPos;
+
+        var color = new Color(0.835f, 1f, 0.792f);
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+        lineRenderer.material.color = color;
+
+        lineRenderer.SetPositions(new[] { transform.position, labelDot.transform.position });
+        Vector3 textOffset = new Vector3(0, 0, 58f);
+
+        if (labelDot.transform.localPosition.z < transform.localPosition.z)
         {
-            if (damageText == null) return;
-            if (labelDot == null) return;
-            if (lineRenderer == null) return;
-
-            var textRect = damageText.transform.GetChild(0).GetComponent<RectTransform>();
-            textRect.localScale = Vector3.one * 23.33f;
-            float xPos = -1167.760f * textRect.localPosition.x;
-            textRect.localPosition = new Vector3(xPos, 5.870f, -189.170f);
-
-            var labelRect = warningPing.GetComponent<RectTransform>();
-            labelRect.sizeDelta = new Vector2(1600, 1584);
-
-            Vector3 parentOffset = (transform.position - parent.position).normalized;
-            Vector3 dotPos = transform.position + parentOffset * 0.35f;
-            labelDot.transform.position = dotPos;
-
-            lineRenderer.SetPositions(new[] { transform.position, labelDot.transform.position });
-            Vector3 textOffset = new Vector3(0, 0, 58f);
-
-            if (labelDot.transform.localPosition.z < transform.localPosition.z)
-            {
-                textOffset *= -1;
-            }
-
-            damageText.transform.localPosition = labelDot.transform.localPosition + textOffset;
+            textOffset *= -1;
         }
+
+        damageText.transform.localPosition = labelDot.transform.localPosition + textOffset;
     }
 
     private void Update()
