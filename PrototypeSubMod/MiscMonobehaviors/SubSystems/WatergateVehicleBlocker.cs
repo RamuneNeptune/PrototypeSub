b63ee0c789dece260847769aa607abc7e7b89d05
Indigocoder1 <caleb@liston.us>
2024-12-23 18:16:42 -0800

    Added player-whitelisted object blockers

--- PrototypeSubMod/MiscMonobehaviors/SubSystems/WatergateVehicleBlocker.cs
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/WatergateVehicleBlocker.cs
@@ -4,26 +4,18 @@ namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
 
 internal class WatergateVehicleBlocker : MonoBehaviour
 {
-    [SerializeField] private BehaviourLOD lod;
-    [SerializeField] private SubRoot subRoot;
     [SerializeField] private Collider[] colliders;
-    [SerializeField] private float timeBetweenChecks = 1;
 
     private void Start()
     {
-        InvokeRepeating(nameof(CheckIfPlayerInVehicle), 0, timeBetweenChecks);
-    }
+        var playerColliders = Player.main.GetComponents<Collider>();
 
-    private void CheckIfPlayerInVehicle()
-    {
-        if (!lod.IsFull()) return;
-
-        bool inVehicle = Player.main.currentMountedVehicle != null;
-        bool inSub = Player.main.currentSub != null && Player.main.currentSub != subRoot;
-
-        foreach (var item in colliders)
+        foreach (Collider collider in colliders)
         {
-            item.isTrigger = !(inVehicle || inSub);
+            foreach (var item in playerColliders)
+            {
+                Physics.IgnoreCollision(collider, item, true);
+            }
         }
     }
 }
