53b558cbbc46f3b91fd2d66bdcc10b2489b6387e
Indigocoder1 <caleb@liston.us>
2024-12-23 20:39:19 -0800

    Tweaked lava larva positions

--- PrototypeSubAssets/Assets/Scripts/LarvaAttachPointPreview.cs
+++ PrototypeSubAssets/Assets/Scripts/LarvaAttachPointPreview.cs
@@ -19,6 +19,9 @@ public class LarvaAttachPointPreview : MonoBehaviour
             }
 
             Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(child.position, 1);
+
+            Gizmos.color = Color.green;
             Gizmos.DrawMesh(cubeMesh, 0, child.position, child.rotation, new Vector3(child.localScale.x, child.localScale.y, child.localScale.z * 0.01f));
 
             Gizmos.color = Color.blue;
