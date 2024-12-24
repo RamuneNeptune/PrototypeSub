049bdb9449154c2a6ae111a2ba836594c74d2e72
Indigocoder1 <caleb@liston.us>
2024-12-23 17:34:35 -0800

    Added lava larvae attach points

---
+++ PrototypeSubAssets/Assets/Scripts/LarvaAttachPointPreview.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LarvaAttachPointPreview : MonoBehaviour
+{
+    public Mesh cubeMesh;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (cubeMesh == null) return;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            Gizmos.DrawMesh(cubeMesh, 0, child.position, child.rotation, new Vector3(child.localScale.x, child.localScale.y, child.localScale.z * 0.01f));
+        }
+    }
+}
