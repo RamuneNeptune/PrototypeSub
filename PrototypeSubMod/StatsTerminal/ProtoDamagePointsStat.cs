7046e5b2068d2429c41fbbc17909ca4409c23a2d
Indigocoder1 <caleb@liston.us>
2024-12-23 16:57:22 -0800

    Added proto warning ping script

--- PrototypeSubMod/StatsTerminal/ProtoDamagePointsStat.cs
+++ PrototypeSubMod/StatsTerminal/ProtoDamagePointsStat.cs
@@ -1,18 +1,42 @@
-﻿using UnityEngine;
+﻿using SubLibrary.CyclopsReferencers;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PrototypeSubMod.StatsTerminal;
 
-internal class ProtoDamagePointsStat : MonoBehaviour, IStatistic
+internal class ProtoDamagePointsStat : MonoBehaviour, IStatistic, ICyclopsReferencer
 {
+    [SerializeField] private BehaviourLOD lod;
     [SerializeField] private Transform damagePointsParent;
     [SerializeField] private Transform displayPointsParent;
+
+    [SerializeField, HideInInspector] public GameObject damagePointPrefab;
+    private Dictionary<GameObject, GameObject> activeDamagePoints = new();
 
     private void Start()
     {
         foreach (Transform child in displayPointsParent.transform)
         {
-            child.gameObject.SetActive(false);
+            child.gameObject.SetActive(true);
         }
+    }
+
+    public void OnCyclopsReferenceFinished(GameObject cyclops)
+    {
+        var iconPrefab = cyclops.transform.Find("HolographicDisplay").GetComponent<CyclopsHolographicHUD>().damageIconPrefab;
+        damagePointPrefab = Instantiate(iconPrefab);
+        damagePointPrefab.SetActive(false);
+
+        var warningPing = damagePointPrefab.GetComponent<CyclopsHolographicHUD_WarningPings>();
+        var protoPing = damagePointPrefab.AddComponent<ProtoWarningPing>();
+        protoPing.warningType = warningPing.warningType;
+        protoPing.damageText = warningPing.damageText;
+        protoPing.labelDot = warningPing.labelDot;
+        protoPing.warningPing = warningPing.warningPing;
+        protoPing.lineRenderer = warningPing.lineRenderer;
+        protoPing.animator = warningPing.animator;
+
+        Destroy(warningPing);
     }
 
     public void UpdateStat() { }
@@ -21,10 +45,26 @@ internal class ProtoDamagePointsStat : MonoBehaviour, IStatistic
     {
         for (int i = 0; i < damagePointsParent.childCount; i++)
         {
-            var childA = damagePointsParent.GetChild(i);
-            var childB = displayPointsParent.GetChild(i);
+            var point = damagePointsParent.GetChild(i).gameObject;
+            var displayChild = displayPointsParent.GetChild(i).gameObject;
+            if (activeDamagePoints.TryGetValue(point, out GameObject controlledPing))
+            {
+                if (!point.activeSelf)
+                {
+                    activeDamagePoints.Remove(point);
+                    controlledPing.GetComponent<ProtoWarningPing>().DespawnIcon();
+                }
+            }
+            else if (point.activeSelf)
+            {
+                var ping = Instantiate(damagePointPrefab, displayChild.transform, false);
+                activeDamagePoints.Add(point, ping);
 
-            childB.gameObject.SetActive(childA.gameObject.activeSelf);
+                var protoPing = ping.GetComponent<ProtoWarningPing>();
+                protoPing.SetLOD(lod);
+                protoPing.SetParent(transform);
+                ping.gameObject.SetActive(true);
+            }
         }
     }
 }
---
+++ PrototypeSubMod/StatsTerminal/ProtoWarningPing.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.StatsTerminal;
+
+internal class ProtoWarningPing : MonoBehaviour
+{
+    public CyclopsHolographicHUD_WarningPings.WarningTypes warningType;
+    public GameObject damageText;
+    public GameObject labelDot;
+    public GameObject warningPing;
+    public LineRenderer lineRenderer;
+    public Animator animator;
+
+    private BehaviourLOD lod;
+    private Transform parent;
+    private bool despawning;
+
+    private void Start()
+    {
+        if (warningType == CyclopsHolographicHUD_WarningPings.WarningTypes.Damage)
+        {
+            if (damageText == null) return;
+            if (labelDot == null) return;
+            if (lineRenderer == null) return;
+
+            var textRect = damageText.transform.GetChild(0).GetComponent<RectTransform>();
+            textRect.localScale = Vector3.one * 23.33f;
+            float xPos = -1167.760f * textRect.localPosition.x;
+            textRect.localPosition = new Vector3(xPos, 5.870f, -189.170f);
+
+            var labelRect = warningPing.GetComponent<RectTransform>();
+            labelRect.sizeDelta = new Vector2(1600, 1584);
+
+            Vector3 parentOffset = (transform.position - parent.position).normalized;
+            Vector3 dotPos = transform.position + parentOffset * 0.35f;
+            labelDot.transform.position = dotPos;
+
+            lineRenderer.SetPositions(new[] { transform.position, labelDot.transform.position });
+            Vector3 textOffset = new Vector3(0, 0, 58f);
+
+            if (labelDot.transform.localPosition.z < transform.localPosition.z)
+            {
+                textOffset *= -1;
+            }
+
+            damageText.transform.localPosition = labelDot.transform.localPosition + textOffset;
+        }
+    }
+
+    private void Update()
+    {
+        if (lod == null || !lod.IsFull()) return;
+
+        warningPing.transform.LookAt(MainCamera.camera.transform.position);
+        if (damageText != null && this.warningType == CyclopsHolographicHUD_WarningPings.WarningTypes.Damage)
+        {
+            labelDot.transform.LookAt(MainCamera.camera.transform.position);
+            labelDot.transform.Rotate(new Vector3(0f, 180f, 0f), Space.Self);
+            damageText.transform.LookAt(MainCamera.camera.transform.position);
+            damageText.transform.Rotate(new Vector3(0f, 180f, 0f), Space.Self);
+
+            lineRenderer.SetPositions(new[] { transform.position, labelDot.transform.position });
+        }
+    }
+
+    public void SetLOD(BehaviourLOD lod)
+    {
+        this.lod = lod;
+    }
+
+    public void SetParent(Transform parent)
+    {
+        this.parent = parent;
+    }
+
+    private void Despawn()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DespawnIcon()
+    {
+        if (despawning) return;
+        despawning = true;
+        animator.SetTrigger("Death");
+        Invoke(nameof(Despawn), 3f);
+    }
+}
