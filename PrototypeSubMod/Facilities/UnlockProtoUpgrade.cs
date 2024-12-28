30dc47b4f8d445b9d8a5937fb7ec0e2681f4be92
Indigocoder1 <caleb@liston.us>
2024-12-27 17:57:11 -0800

    Added upgrade categories

---
+++ PrototypeSubMod/Facilities/UnlockProtoUpgrade.cs
@@ -0,0 +1,43 @@
+﻿using PrototypeSubMod.Utility;
+using UnityEngine;
+
+namespace PrototypeSubMod.Facilities;
+
+internal class UnlockProtoUpgrade : MonoBehaviour
+{
+    [SerializeField] private MultipurposeAlienTerminal terminal;
+    [SerializeField] private ProtoUpgradeCategory upgradeCategory;
+    [SerializeField] private DummyTechType techType;
+    [SerializeField] private string encyclopediaKey;
+
+    private bool unlocked;
+
+    private void Start()
+    {
+        unlocked = KnownTech.Contains(techType.TechType);
+        terminal.onTerminalInteracted += UnlockTechType;
+
+        if (unlocked)
+        {
+            terminal.ForceInteracted();
+        }
+    }
+
+    public void UnlockTechType()
+    {
+        if (unlocked) return;
+
+        KnownTech.Add(techType.TechType);
+
+        int lockedCount = upgradeCategory.GetLockedUpgrades().Count;
+        string message = Language.main.GetFormat("ProtoUpgradeUnlocked", Language.main.Get(techType.TechType), lockedCount);
+        ErrorMessage.AddError(message);
+
+        if (!string.IsNullOrEmpty(encyclopediaKey))
+        {
+            PDAEncyclopedia.Add(encyclopediaKey, true);
+        }
+
+        unlocked = true;
+    }
+}
---
+++ PrototypeSubMod/Utility/ProtoUpgradeCategory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeSubMod.Utility;
+
+[CreateAssetMenu(fileName = "UpgradeCategory", menuName = "Prototype Sub/Upgrade Category")]
+internal class ProtoUpgradeCategory : ScriptableObject
+{
+    public string localizationKey;
+    public DummyTechType[] ownedTechTypes;
+
+    public TechType[] GetTechTypes()
+    {
+        TechType[] types = new TechType[ownedTechTypes.Length];
+        for (int i = 0; i < ownedTechTypes.Length; i++)
+        {
+            types[i] = ownedTechTypes[i].TechType;
+        }
+
+        return types;
+    }
+
+    public List<TechType> GetLockedUpgrades()
+    {
+        List<TechType> lockedTechs = new();
+
+        foreach (var item in ownedTechTypes)
+        {
+            if (!KnownTech.Contains(item.TechType))
+            {
+                lockedTechs.Add(item.TechType);
+            }
+        }
+
+        return lockedTechs;
+    }
+
+    public string GetName()
+    {
+        return Language.main.Get(localizationKey);
+    }
+}
