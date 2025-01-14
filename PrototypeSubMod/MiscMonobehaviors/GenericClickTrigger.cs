678e6011b124e6ae92c98598c80c451ad41b27a2
Indigocoder1 <caleb@liston.us>
2025-01-13 19:58:15 -0800

    Added generic click trigger

---
+++ PrototypeSubMod/MiscMonobehaviors/GenericClickTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+internal class GenericClickTrigger : MonoBehaviour, IHandTarget
+{
+    [SerializeField] private UnityEvent onClick;
+
+    public void OnHandClick(GUIHand hand)
+    {
+        onClick?.Invoke();
+    }
+
+    public void OnHandHover(GUIHand hand)
+    {
+        
+    }
+}
