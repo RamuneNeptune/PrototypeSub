9f3321acbdbb0f52589cc4b9102e76ed22822fa3
Indigocoder1 <caleb@liston.us>
2025-01-18 16:21:30 -0800

    Added exterior colliders to engine facility

---
+++ PrototypeSubAssets/Assets/Scripts/ApplyMaterial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ApplyMaterial : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private bool remove;
+
+    private void Update()
+    {
+        if (!remove) return;
+        remove = false;
+        var rends = GetComponentsInChildren<MeshRenderer>(true);
+        foreach (var rend in rends)
+        {
+            rend.material = material;
+        }
+    }
+}
