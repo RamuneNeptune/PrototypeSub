75d3fd195b8586126a54e69fceced035deea3268
Indigocoder1 <caleb@liston.us>
2025-01-18 19:35:18 -0800

    Added SetRBCenterOfMass script

---
+++ PrototypeSubMod/MiscMonobehaviors/SetRBCenterOfMass.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+internal class SetRBCenterOfMass : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 localOffset;
+
+    private void Start()
+    {
+        rb.centerOfMass = localOffset;
+    }
+}
