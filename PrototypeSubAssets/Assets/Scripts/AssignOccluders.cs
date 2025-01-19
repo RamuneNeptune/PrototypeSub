07f0702bdb94190bffbb9dbc38fda838f05d07aa
Indigocoder1 <caleb@liston.us>
2025-01-19 11:23:07 -0800

    Added culling occlusion to engine facility

---
+++ PrototypeSubAssets/Assets/Scripts/AssignOccluders.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class AssignOccluders : MonoBehaviour
+{
+    [SerializeField] private bool remove;
+
+    private void Update()
+    {
+        if (!remove) return;
+        remove = false;
+        var occluders = GetComponentsInChildren<CullingOccluder>(true);
+        foreach (var item in occluders)
+        {
+            item.occluderRenderer = item.GetComponent<Renderer>();
+            item.meshFilter = item.GetComponent<MeshFilter>();
+        }
+    }
+}
