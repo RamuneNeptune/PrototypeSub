3626d72b6a13fa523d0cac74dd5fb24b814f2571
Indigocoder1 <caleb@liston.us>
2025-01-18 18:48:08 -0800

    Made it so child objects can be grabbed via propulsion cannon

---
+++ PrototypeSubMod/Patches/Utils_Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.MiscMonobehaviors;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(UWE.Utils))]
+internal class Utils_Patches
+{
+    [HarmonyPatch(nameof(UWE.Utils.GetEntityRoot)), HarmonyPostfix]
+    private static void GetEntityRoot_Postfix(GameObject go, ref GameObject __result)
+    {
+        if (!go.GetComponent<MarkAsEntityRoot>()) return;
+
+        __result = go;
+    }
+}
