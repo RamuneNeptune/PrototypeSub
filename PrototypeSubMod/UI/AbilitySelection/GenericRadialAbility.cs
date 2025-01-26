76d600cdef44bd46636e52667a6d99b49801f113
Indigocoder1 <caleb@liston.us>
2025-01-26 10:23:10 -0800

    Added generic radial ability

---
+++ PrototypeSubMod/UI/AbilitySelection/GenericRadialAbility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrototypeSubMod.UI.AbilitySelection;
+
+internal class GenericRadialAbility : MonoBehaviour, IAbilityIcon
+{
+    [SerializeField] private bool showAbility = true;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private UnityEvent onActivated;
+
+    public bool GetShouldShow() => showAbility;
+    public Sprite GetSprite() => sprite;
+
+    public void OnActivated()
+    {
+        onActivated?.Invoke();
+    }
+
+    public void OnSelectedChanged(bool changed) { }
+}
