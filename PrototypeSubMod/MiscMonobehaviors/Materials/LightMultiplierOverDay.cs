e9d1e5c52364a8cff6b6c21fb92ece8e8934958b
Indigocoder1 <caleb@liston.us>
2025-02-02 18:08:25 -0800

    Added light multiplier over day

---
+++ PrototypeSubMod/MiscMonobehaviors/Materials/LightMultiplierOverDay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.Materials;
+
+internal class LightMultiplierOverDay : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve multiplierOverDay;
+    [SerializeField] private Renderer renderer;
+
+    private void FixedUpdate()
+    {
+        float multiplier = multiplierOverDay.Evaluate(DayNightCycle.main.GetDayScalar());
+        renderer.material.SetFloat("_NightMultiplier", multiplier);
+    }
+}
