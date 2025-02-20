9535c37c11ee0bc1a3d13f49bbf1eb0a1f19de42
Indigocoder1 <caleb@liston.us>
2025-02-19 19:02:11 -0800

    Add fog over day script

---
+++ PrototypeSubMod/MiscMonobehaviors/Materials/FogDistOverDay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.Materials
+{
+    internal class FogDistOverDay : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve distOverDay;
+        [SerializeField] private Renderer renderer;
+
+        private void FixedUpdate()
+        {
+            float dist = distOverDay.Evaluate(DayNightCycle.main.GetDayScalar());
+            renderer.material.SetFloat("_FogMaxDist", dist);
+        }
+    }
+}
