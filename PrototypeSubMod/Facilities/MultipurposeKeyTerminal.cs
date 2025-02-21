3b4c40ef9ab241a83b0afbd45b9048e6bc0c06b4
Indigocoder1 <caleb@liston.us>
2025-02-20 20:03:48 -0800

    Add multipurpose key terminal script

---
+++ PrototypeSubMod/Facilities/MultipurposeKeyTerminal.cs
@@ -0,0 +1,79 @@
+﻿using Nautilus.Handlers;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UWE;
+
+namespace PrototypeSubMod.Facilities;
+
+internal class MultipurposeKeyTerminal : MonoBehaviour
+{
+    private static GameObject KeyTerminalPrefab;
+
+    [SerializeField] private string techType;
+    [SerializeField] private Texture2D replacementSprite;
+    [SerializeField] private UnityEvent onInteracted;
+
+    private IEnumerator Start()
+    {
+        if (KeyTerminalPrefab)
+        {
+            SpawnPrefab(KeyTerminalPrefab);
+            yield break;
+        }
+
+        var prefabRequest = PrefabDatabase.GetPrefabAsync("c718547d-fe06-4247-86d0-efd1e3747af0");
+
+        yield return prefabRequest;
+
+        GameObject prefab = null;
+        if (!prefabRequest.TryGetPrefab(out prefab)) throw new Exception("Error retrieving precursor key terminal prefab!");
+
+        prefab.SetActive(false);
+        KeyTerminalPrefab = prefab;
+        SpawnPrefab(KeyTerminalPrefab);
+    }
+
+    private void SpawnPrefab(GameObject prefab)
+    {
+        TechType techType = TechType.None;
+        if (!Enum.TryParse(this.techType, out techType))
+        {
+            throw new Exception($"Unable to parse tech type with name {this.techType}");
+        }
+
+        var keyType = CreateOrRetrieveKeyType(techType);
+        var instance = Instantiate(prefab, transform, false);
+
+        instance.transform.localPosition = Vector3.zero;
+        instance.transform.localRotation = Quaternion.identity;
+        instance.transform.localScale = Vector3.one;
+        instance.SetActive(true);
+
+        var keyTerminal = instance.GetComponent<PrecursorKeyTerminal>();
+        keyTerminal.acceptKeyType = keyType;
+        var glyphRenderer = keyTerminal.transform.Find("Precursor_key_terminal_01/glyph/Face_F").GetComponent<Renderer>();
+
+        glyphRenderer.material.mainTexture = replacementSprite;
+
+        Destroy(instance.GetComponent<PrefabIdentifier>());
+    }
+
+    private PrecursorKeyTerminal.PrecursorKeyType CreateOrRetrieveKeyType(TechType type)
+    {
+        if (Enum.TryParse(type.ToString(), true, out PrecursorKeyTerminal.PrecursorKeyType result))
+        {
+            return result;
+        }
+
+        var builder = EnumHandler.AddEntry<PrecursorKeyTerminal.PrecursorKeyType>(type.ToString());
+        return builder.Value;
+    }
+
+    // Called via BroadcastMessage on PrecursorKeyTerminal
+    public void ToggleDoor()
+    {
+        onInteracted?.Invoke();
+    }
+}
