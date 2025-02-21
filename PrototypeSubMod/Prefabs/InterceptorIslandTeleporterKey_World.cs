e39c50e4344a944d0d7ef42879044f96922d0b8a
Indigocoder1 <caleb@liston.us>
2025-02-20 20:54:21 -0800

    Fix various tablet issues

--- PrototypeSubMod/Prefabs/InterceptorIslandTeleporterKey_World.cs
+++ PrototypeSubMod/Prefabs/InterceptorIslandTeleporterKey_World.cs
@@ -1,4 +1,5 @@
 ﻿using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
 using Nautilus.Assets.PrefabTemplates;
 using UnityEngine;
 
@@ -11,7 +12,6 @@ internal class InterceptorIslandTeleporterKey_World
     public static void Register()
     {
         prefabInfo = PrefabInfo.WithTechType("ProtoIslandTeleporterKey", null, null, "English")
-            .WithSizeInInventory(new Vector2int(2, 2))
             .WithIcon(Plugin.AssetBundle.LoadAsset<Sprite>("AlienFramework_Icon"));
 
         var prefab = new CustomPrefab(prefabInfo);
@@ -20,18 +20,23 @@ internal class InterceptorIslandTeleporterKey_World
         {
             Texture2D replacementGlyph = Plugin.AssetBundle.LoadAsset<Texture2D>("InterceptorGlyph");
             var rend1 = gameObject.transform.Find("Model/Rig_J/precursor_key_C_02_symbol_05").GetComponent<Renderer>();
-            var rend2 = gameObject.transform.Find("/ViewModel/Rig_J/precursor_key_C_02_symbol_05").GetComponent<Renderer>();
+            var rend2 = gameObject.transform.Find("ViewModel/Rig_J/precursor_key_C_02_symbol_05").GetComponent<Renderer>();
 
             var tempMats = rend1.materials;
-            tempMats[1].mainTexture = replacementGlyph;
+            tempMats[1].SetTexture("_MainTex", replacementGlyph);
+            tempMats[1].SetTexture("_SpecTex", replacementGlyph);
+            tempMats[1].SetTexture("_Illum", replacementGlyph);
             rend1.materials = tempMats;
 
             tempMats = rend2.materials;
-            tempMats[1].mainTexture = replacementGlyph;
+            tempMats[1].SetTexture("_MainTex", replacementGlyph);
+            tempMats[1].SetTexture("_SpecTex", replacementGlyph);
+            tempMats[1].SetTexture("_Illum", replacementGlyph);
             rend2.materials = tempMats;
         };
 
         prefab.SetGameObject(cloneTemplate);
+        prefab.SetEquipment(EquipmentType.Hand);
 
         prefab.Register();
     }
