88d652ffabadae44d9b3d70ed90abccc52d4c5c8
Indigocoder1 <caleb@liston.us>
2025-02-21 19:03:08 -0800

    Fix NRE on PrecursorKeyTerminal.Start

---
+++ PrototypeSubMod/Patches/PrecursorKeyTerminal_Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(PrecursorKeyTerminal))]
+internal class PrecursorKeyTerminal_Patches
+{
+    [HarmonyPatch(nameof(PrecursorKeyTerminal.Start)), HarmonyPrefix]
+    private static bool Start_Prefix(PrecursorKeyTerminal __instance)
+    {
+        return (int)__instance.acceptKeyType < __instance.keyMats.Length;
+    }
+}
