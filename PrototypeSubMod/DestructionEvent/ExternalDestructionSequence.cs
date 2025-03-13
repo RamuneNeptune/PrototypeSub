094fcf036cc5b59194000113a94192b219b79cf6
Indigocoder1 <caleb@liston.us>
2025-03-12 17:01:35 -0700

    Fix warp out fx not spawning

--- PrototypeSubMod/DestructionEvent/ExternalDestructionSequence.cs
+++ PrototypeSubMod/DestructionEvent/ExternalDestructionSequence.cs
@@ -1,17 +1,34 @@
 ﻿using PrototypeSubMod.IonGenerator;
+using System.Collections;
 using UnityEngine;
 
 namespace PrototypeSubMod.DestructionEvent;
 
 internal class ExternalDestructionSequence : DestructionSequence
 {
+    [SerializeField] private Transform warpOutSpawnPos;
     [SerializeField] private EmpSpawner empSpawner;
     [SerializeField] private float disableElectronicsTime;
+
+    private GameObject warpOutFX;
+
+    private IEnumerator Start()
+    {
+        var task = CraftData.GetPrefabForTechTypeAsync(TechType.Warper);
+        yield return task;
+
+        var result = task.GetResult();
+        var warper = result.GetComponent<Warper>();
+        warpOutFX = warper.warpOutEffectPrefab;
+    }
 
     public override void StartSequence(SubRoot subRoot)
     {
         empSpawner.FireEMP(disableElectronicsTime);
-        subRoot.gameObject.SetActive(false);
-        subRoot.transform.position = new Vector3(0, 100, 0);
+        //subRoot.gameObject.SetActive(false);
+        //subRoot.transform.position = new Vector3(0, 100, 0);
+
+        var fx = Instantiate(warpOutFX, warpOutSpawnPos.position, warpOutSpawnPos.rotation);
+        fx.transform.localScale = Vector3.one * 8f;
     }
 }
