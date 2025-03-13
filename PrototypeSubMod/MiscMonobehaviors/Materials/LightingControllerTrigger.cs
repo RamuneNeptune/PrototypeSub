29d9cfb7532215d01264955c0cdce7ee12920a6c
Indigocoder1 <caleb@liston.us>
2025-03-12 18:40:43 -0700

    Add lighting controller to engine facility

---
+++ PrototypeSubMod/MiscMonobehaviors/Materials/LightingControllerTrigger.cs
@@ -0,0 +1,30 @@
+﻿using PrototypeSubMod.Facilities;
+using Story;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.Materials;
+
+internal class LightingControllerTrigger : MonoBehaviour
+{
+    [SerializeField] private MultipurposeAlienTerminal terminal;
+    [SerializeField] private LightingController controller;
+    [SerializeField] private string storyGoalCheck;
+    [SerializeField] private bool disableAtStart = true;
+    [SerializeField] private LightingController.LightingState targetState;
+    [SerializeField] private float transitionTime;
+
+    private void Start()
+    {
+        if (!string.IsNullOrEmpty(storyGoalCheck) && StoryGoalManager.main.IsGoalComplete(storyGoalCheck))
+        {
+            controller.SnapToState((int)targetState);
+        }
+        else
+        {
+            terminal.onTerminalInteracted += () =>
+            {
+                controller.LerpToState((int)targetState, transitionTime);
+            };
+        }
+    }
+}
