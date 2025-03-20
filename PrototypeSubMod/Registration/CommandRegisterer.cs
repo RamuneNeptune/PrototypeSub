fa0fcec97512102cc6d50dc063256c5d1b5d4cae
Indigocoder1 <caleb@liston.us>
2025-03-19 20:01:46 -0700

    Add hull facility goto command

--- PrototypeSubMod/Registration/CommandRegisterer.cs
+++ PrototypeSubMod/Registration/CommandRegisterer.cs
@@ -10,5 +10,6 @@ internal class CommandRegisterer
         ConsoleCommandsHandler.AddGotoTeleportPosition("interceptorfacility", new Vector3(547, -709, 955));
         ConsoleCommandsHandler.AddGotoTeleportPosition("defensefacility", new Vector3(689, -483, -1404f));
         ConsoleCommandsHandler.AddGotoTeleportPosition("enginefacility", new Vector3(306, -1156, 131f));
+        ConsoleCommandsHandler.AddGotoTeleportPosition("hullfacility", new Vector3(-1260, -614, -352f));
     }
 }
