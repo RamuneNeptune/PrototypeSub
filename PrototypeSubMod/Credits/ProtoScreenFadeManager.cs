6865f5fe2cb57c48526e3fc9d22bf350d111ecf4
Indigocoder1 <caleb@liston.us>
2025-03-21 19:58:17 -0700

    Fix incorrect fading calculations

--- PrototypeSubMod/Credits/ProtoScreenFadeManager.cs
+++ PrototypeSubMod/Credits/ProtoScreenFadeManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace PrototypeSubMod.Credits;
@@ -10,13 +11,19 @@ public class ProtoScreenFadeManager : MonoBehaviour
     private bool fading;
     private float currentFadeTime;
     private float targetFadeTime;
-    private Color targetColor;
+    private Color targetColor = Color.clear;
+    private Color previousColor;
+
+    private void Start()
+    {
+        fadeImage.color = Color.clear;
+    }
 
     private void Update()
     {
         if (!fading) return;
 
-        if (currentFadeTime <   targetFadeTime)
+        if (currentFadeTime < targetFadeTime)
         {
             currentFadeTime += Time.deltaTime;
         }
@@ -25,22 +32,24 @@ public class ProtoScreenFadeManager : MonoBehaviour
             fading = false;
         }
         
-        fadeImage.color = Color.Lerp(fadeImage.color, targetColor, currentFadeTime / targetFadeTime);
+        fadeImage.color = Color.Lerp(previousColor, targetColor, currentFadeTime / targetFadeTime);
     }
 
     public void FadeIn(float duration)
     {
         fading = true;
-        currentFadeTime = duration;
+        currentFadeTime = 0;
         targetFadeTime = duration;
+        previousColor = targetColor;
         targetColor = Color.black;
     }
     
     public void FadeOut(float duration)
     {
         fading = true;
-        currentFadeTime = duration;
+        currentFadeTime = 0;
         targetFadeTime = duration;
+        previousColor = targetColor;
         targetColor = new Color(0, 0, 0, 0);
     }
 }
