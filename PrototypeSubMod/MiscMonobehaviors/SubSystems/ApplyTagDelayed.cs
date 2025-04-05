2b95995032041f85997702c0c002926ac47ba0f1
Indigocoder1 <caleb@liston.us>
2025-04-05 14:56:46 -0700

    Make interfloor teleporters blocked building spots

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/ApplyTagDelayed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class ApplyTagDelayed : MonoBehaviour
+{
+    [SerializeField] private string tag;
+    [SerializeField] private int frameDelay = 2;
+    
+    private IEnumerator Start()
+    {
+        for (int i = 0; i < frameDelay; i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.tag = tag;
+    }
+}
