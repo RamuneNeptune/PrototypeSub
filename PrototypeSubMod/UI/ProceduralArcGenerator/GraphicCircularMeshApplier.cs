454f68e88959a6f265afefd938867ce3cee63f56
Indigocoder1 <caleb@liston.us>
2025-04-18 17:35:41 -0700

    Update circular mesh generator to support graphics

---
+++ PrototypeSubMod/UI/ProceduralArcGenerator/GraphicCircularMeshApplier.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.UI.ProceduralArcGenerator;
+
+public class GraphicCircularMeshApplier : Graphic
+{
+    [SerializeField] private CircularMeshGenerator meshGenerator;
+    [SerializeField] private bool updateMesh;
+
+    private void OnValidate()
+    {
+        if (updateMesh)
+        {
+            
+        }
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        var mesh = meshGenerator.GenerateMesh();
+        base.OnPopulateMesh(vh);
+        vh.Clear();
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            var vert = UIVertex.simpleVert;
+            vert.position = mesh.vertices[i];
+            vert.normal = mesh.normals[i];
+            vh.AddVert(vert);
+        }
+
+        for (int i = 0; i < mesh.triangles.Length; i += 3)
+        {
+            var index0 = mesh.triangles[i];
+            var index1 = mesh.triangles[i + 1];
+            var index2 = mesh.triangles[i + 2];
+            vh.AddTriangle(index0, index1, index2);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!updateMesh) return;
+
+        UpdateGeometry();
+    }
+}
