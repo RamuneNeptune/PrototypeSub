833fcc922552acc8d2d7b5354c7b08902f51d8cf
Indigocoder1 <caleb@liston.us>
2025-04-19 09:38:11 -0700

    Add depth UI

---
+++ PrototypeSubMod/UI/DepthDisplay/DepthDiplay.cs
@@ -0,0 +1,24 @@
+﻿using SubLibrary.UI;
+using UnityEngine;
+
+namespace PrototypeSubMod.UI.DepthDisplay;
+
+public class ProtoDepthDisplay : MonoBehaviour, IUIElement
+{
+    [SerializeField] private CrushDamage crushDamage;
+    [SerializeField] private Transform[] leftBars;
+    [SerializeField] private Transform[] rightBars;
+    
+    public void UpdateUI()
+    {
+        int segmentCount = Mathf.FloorToInt(crushDamage.GetDepth() / crushDamage.crushDepth * 10);
+        for (int i = 0; i < leftBars.Length; i++)
+        {
+            bool active = (leftBars.Length - i) <= segmentCount;
+            leftBars[i].gameObject.SetActive(active);
+            rightBars[i].gameObject.SetActive(active);
+        }
+    }
+
+    public void OnSubDestroyed() { }
+}
