18b413c3e8510922aa1db8e1cc32cefa0a186316
Indigocoder1 <caleb@liston.us>
2025-04-21 16:02:05 -0700

    Fix fire spawns being disabled

---
+++ PrototypeSubMod/Patches/PrefabSpawnBase_Patches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.MiscMonobehaviors.PrefabRetrievers;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(PrefabSpawnBase))]
+public class PrefabSpawnBase_Patches
+{
+    [HarmonyPatch(nameof(PrefabSpawnBase.Awake)), HarmonyPrefix]
+    private static bool Awake_Prefix(PrefabSpawnBase __instance)
+    {
+        if (!__instance.HasValidPrefab() && __instance.TryGetComponent(out FirePrefabSetter _)) return false;
+
+        return true;
+    }
+    
+    [HarmonyPatch(nameof(PrefabSpawnBase.Start)), HarmonyPrefix]
+    private static bool Start_Prefix(PrefabSpawnBase __instance)
+    {
+        if (!__instance.HasValidPrefab() && __instance.TryGetComponent(out FirePrefabSetter _)) return false;
+
+        return true;
+    }
+}
