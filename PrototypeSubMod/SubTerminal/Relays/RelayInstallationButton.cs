6d980583b36c87ab9e8f972242bd0af76b131190
Indigocoder1 <caleb@liston.us>
2025-04-26 10:00:36 -0700

    Add relay installation to buttons

--- PrototypeSubMod/SubTerminal/Relays/RelayInstallationButton.cs
+++ PrototypeSubMod/SubTerminal/Relays/RelayInstallationButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using PrototypeSubMod.Utility;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace PrototypeSubMod.SubTerminal.Relays;
@@ -6,12 +7,47 @@ namespace PrototypeSubMod.SubTerminal.Relays;
 public class RelayInstallationButton : MonoBehaviour
 {
     [SerializeField] private Image[] images;
+    [SerializeField] private RelayInstallationManager installationManager;
+    [SerializeField] private DummyTechType relayUpgradeTechType;
+    [SerializeField] private RocketBuilderTooltip tooltip;
 
-    public void SetColor(float fillCol)
+    private bool tooltipActive;
+    private bool buttonEnabled;
+    
+    private void Start()
     {
+        tooltip.rocketTechType = relayUpgradeTechType.TechType;
+        InvokeRepeating(nameof(UpdateTooltipActive), 0, 1);
+    }
+    
+    public void SetEnabled(bool enabled)
+    {
+        buttonEnabled = enabled;
+        float fillCol = enabled ? 0.8f : 0.5f;
+        
         foreach (var image in images)
         {
             image.color = new Color(fillCol, fillCol,fillCol);
         }
+
+        if (!enabled)
+        {
+            tooltip.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateTooltipActive()
+    {
+        if (!buttonEnabled) return;
+        
+        tooltipActive = (Player.main.transform.position - transform.position).sqrMagnitude < 9f;
+        tooltip.gameObject.SetActive(tooltipActive);
+    }
+
+    public void OnClick()
+    {
+        if (!CrafterLogic.ConsumeResources(relayUpgradeTechType.TechType)) return;
+
+        installationManager.InstallRelay();
     }
 }
--- PrototypeSubMod/SubTerminal/Relays/RelayInstallationManager.cs
+++ PrototypeSubMod/SubTerminal/Relays/RelayInstallationManager.cs
@@ -13,6 +13,7 @@ public class RelayInstallationManager : MonoBehaviour
     private void Start()
     {
         occupiedHandler.onHasSubChanged.AddListener(OnHasSubChanged);
+        UpdateIcons();
     }
 
     private void OnHasSubChanged()
@@ -25,10 +26,26 @@ public class RelayInstallationManager : MonoBehaviour
 
     private void UpdateIcons()
     {
+        if (powerSystem == null) return;
+        
         for (int i = 0; i < installationButtons.Length; i++)
         {
             var button = installationButtons[i];
-            button.SetColor(i >= powerSystem.GetAllowedSourcesCount() - 2 ? 0.8f : 0.5f);
+            button.SetEnabled(i == powerSystem.GetAllowedSourcesCount() - 2);
         }
+    }
+
+    public void InstallRelay()
+    {
+        if (powerSystem == null)
+        {
+            Plugin.Logger.LogError("Tried to install relay without power system reference");
+            return;
+        }
+        
+        powerSystem.SetAllowedSourcesCount(powerSystem.GetAllowedSourcesCount() + 1);
+        ErrorMessage.AddError($"Installed new relay. Current count = {powerSystem.GetAllowedSourcesCount()}");
+        
+        UpdateIcons();
     }
 }
