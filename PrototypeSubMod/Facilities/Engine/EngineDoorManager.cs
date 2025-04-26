98ae389ec51a29bb4b42a673d5c7a390090bd242
Indigocoder1 <caleb@liston.us>
2025-04-25 18:41:00 -0700

    Update battery prop load distance

---
+++ PrototypeSubMod/Facilities/Engine/EngineDoorManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Engine;
+
+public class EngineDoorManager : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    private IEnumerator Start()
+    {
+        rb.isKinematic = true;
+        yield return new WaitUntil(() => LargeWorldStreamer.main.IsWorldSettled());
+        
+        rb.isKinematic = false;
+    }
+}
