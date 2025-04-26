704a527be932837144a1facba69b20d1fc228932
Indigocoder1 <caleb@liston.us>
2025-04-25 20:59:04 -0700

    Add relay screen to PPT

---
+++ PrototypeSubMod/SubTerminal/RelayInstallationManager.cs
@@ -0,0 +1,23 @@
+﻿using PrototypeSubMod.PowerSystem;
+using UnityEngine;
+
+namespace PrototypeSubMod.SubTerminal;
+
+public class RelayInstallationManager : MonoBehaviour
+{
+    [SerializeField] private MoonpoolOccupiedHandler occupiedHandler;
+
+    private PrototypePowerSystem powerSystem;
+    
+    private void Start()
+    {
+        occupiedHandler.onHasSubChanged.AddListener(OnHasSubChanged);
+    }
+
+    private void OnHasSubChanged()
+    {
+        if (!occupiedHandler.MoonpoolHasSub) return;
+        
+        powerSystem = occupiedHandler.SubInMoonpool.GetComponentInChildren<PrototypePowerSystem>();
+    }
+}
