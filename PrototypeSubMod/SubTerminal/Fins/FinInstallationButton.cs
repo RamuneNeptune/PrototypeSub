2442dff4edeaef8683f4e5f56d8d214770ce04a0
Indigocoder1 <caleb@liston.us>
2025-04-27 11:32:18 -0700

    Add new fin upgrade UI

---
+++ PrototypeSubMod/SubTerminal/Fins/FinInstallationButton.cs
@@ -0,0 +1,77 @@
+﻿using PrototypeSubMod.Utility;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.SubTerminal.Fins;
+
+public class FinInstallationButton : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private DummyTechType relayUpgradeTechType;
+    [SerializeField] private RocketBuilderTooltip tooltip;
+    [SerializeField] private UnityEvent onClick;
+    [SerializeField] private Sprite constructedSprite;
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite hoveredSprite;
+
+    private bool tooltipActive;
+    private bool canBuild;
+    private bool hovered;
+    
+    private void Start()
+    {
+        tooltip.rocketTechType = relayUpgradeTechType.TechType;
+        InvokeRepeating(nameof(UpdateTooltipActive), 0, 1);
+    }
+    
+    public void SetCanBuild(bool canBuild)
+    {
+        this.canBuild = canBuild;
+        
+        if (!canBuild)
+        {
+            tooltip.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetConstructed(bool constructed)
+    {
+        if (!constructed) return;
+        
+        image.sprite = constructedSprite;
+    }
+
+    public void OnPointerEnter(BaseEventData eventData)
+    {
+        hovered = true;
+    }
+    
+    public void OnPointerExit(BaseEventData eventData)
+    {
+        hovered = false;
+    }
+    
+    private void Update()
+    {
+        if (Time.deltaTime == 0 || !canBuild) return;
+        
+        image.sprite = hovered ?  hoveredSprite : normalSprite;
+    }
+
+    private void UpdateTooltipActive()
+    {
+        if (!canBuild) return;
+        
+        tooltipActive = (Player.main.transform.position - transform.position).sqrMagnitude < 9f;
+        tooltip.gameObject.SetActive(tooltipActive);
+    }
+
+    public void OnClick()
+    {
+        if (!CrafterLogic.ConsumeResources(relayUpgradeTechType.TechType)) return;
+
+        onClick?.Invoke();
+    }
+}
--- PrototypeSubMod/SubTerminal/Fins/FinInstallationManager.cs
+++ PrototypeSubMod/SubTerminal/Fins/FinInstallationManager.cs
@@ -7,7 +7,7 @@ namespace PrototypeSubMod.SubTerminal.Fins;
 public class FinInstallationManager : MonoBehaviour
 {
     [SerializeField] private MoonpoolOccupiedHandler occupiedHandler;
-    [SerializeField] private SubUpgradeInstallationButton[] installationButtons;
+    [SerializeField] private FinInstallationButton[] installationButtons;
 
     private ProtoFinsManager finsManager;
     
@@ -32,7 +32,8 @@ public class FinInstallationManager : MonoBehaviour
         for (int i = 0; i < installationButtons.Length; i++)
         {
             var button = installationButtons[i];
-            button.SetEnabled(i == finsManager.GetInstalledFinCount());
+            button.SetCanBuild(i == finsManager.GetInstalledFinCount());
+            button.SetConstructed(i < finsManager.GetInstalledFinCount());
         }
     }
 
