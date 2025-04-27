08d2aa8f3d8d553918f60dfcc67c1eca66ce912a
Indigocoder1 <caleb@liston.us>
2025-04-27 12:26:40 -0700

    Create worm spine manager & spine prefab

---
+++ PrototypeSubMod/Facilities/Hull/ProtoWormSpineManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Hull;
+
+public class ProtoWormSpineManager : MonoBehaviour
+{
+    [SerializeField] private Transform segmentsParent;
+    [SerializeField] private GameObject spineSegmentPrefab;
+    [SerializeField] private Vector3 initialLocalPos;
+    [SerializeField] private Vector3 incrementPerSpine;
+    [SerializeField] private int spineSegmentCount;
+
+    private void Start()
+    {
+        for (int i = 0; i < spineSegmentCount; i++)
+        {
+            var spine = Instantiate(spineSegmentPrefab, segmentsParent);
+            spine.transform.localPosition = initialLocalPos + incrementPerSpine * i;
+        }
+    }
+}
