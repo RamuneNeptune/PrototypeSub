5dd373a4f1d9ead8aeeb849bffec62498cc05810
Indigocoder1 <caleb@liston.us>
2025-05-02 16:33:48 -0700

    Add stairs manager

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/ProtoStairsManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class ProtoStairsManager : MonoBehaviour
+{
+    private static readonly int StairsActive = Animator.StringToHash("StairsActive");
+    
+    [SerializeField] private Animator stairsAnimator;
+    
+    private bool stairsActive;
+    
+    private void Start()
+    {
+        Player.main.playerRespawnEvent.AddHandler(gameObject, OnRespawn);
+    }
+
+    private void OnRespawn(Player player)
+    {
+        SetStairsActive(false);
+    }
+
+    public void SetStairsActive(bool active)
+    {
+        stairsAnimator.SetBool(StairsActive, active);
+        stairsActive = active;
+    }
+
+    public void ToggleStairsActive()
+    {
+        SetStairsActive(!stairsActive);
+    }
+}
