56d591436caec032915427a5aacebe74dc0724dc
Indigocoder1 <caleb@liston.us>
2025-05-03 11:57:27 -0700

    Make occluder renderers disabled after bounds cached

---
+++ PrototypeSubMod/Patches/CullingOccluder_Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(CullingOccluder))]
+public class CullingOccluder_Patches
+{
+    [HarmonyPatch(nameof(CullingOccluder.Start)), HarmonyPostfix]
+    private static void Start_Postfix(CullingOccluder __instance)
+    {
+        if (__instance.TryGetComponent(out DisableRendAfterBoundsCached _))
+        {
+            __instance.occluderRenderer.enabled = false;
+        }
+    }
+}
