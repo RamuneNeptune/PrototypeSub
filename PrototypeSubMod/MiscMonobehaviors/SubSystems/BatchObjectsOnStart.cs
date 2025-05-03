e2639354015aa8d18eacd598a8a7c42e4072cab7
Indigocoder1 <caleb@liston.us>
2025-05-03 10:39:09 -0700

    Add BatchObjectsOnStart

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/BatchObjectsOnStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class BatchObjectsOnStart : MonoBehaviour
+{
+    [SerializeField] private GameObject staticBatchRoot;
+
+    private void Start()
+    {
+        StaticBatchingUtility.Combine(staticBatchRoot);
+    }
+}
