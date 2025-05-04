edcf50d198be00849bb494625669affb5a0a79d6
Indigocoder1 <caleb@liston.us>
2025-05-04 08:56:37 -0700

    Apply SN shaders to hull facility

---
+++ PrototypeSubMod/Facilities/Hull/ApplySNMaterialsBatchable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Hull;
+
+public class ApplySNMaterialsBatchable : MonoBehaviour
+{
+    [SerializeField] private GameObject applyTo;
+    [SerializeField] private float shininess = 4;
+    [SerializeField] private float specularIntensity = 1;
+    [SerializeField] private float glowStrength = 1;
+
+    private void OnValidate()
+    {
+        if (!applyTo) applyTo = gameObject;
+    }
+
+    private void Start()
+    {
+        foreach (var rend in GetComponentsInChildren<Renderer>(true))
+        {
+            foreach (var material in rend.sharedMaterials)
+            {
+                if (material.shader == MaterialUtils.Shaders.MarmosetUBER) continue;
+                
+                MaterialUtils.ApplyUBERShader(material, shininess, specularIntensity, glowStrength, MaterialUtils.MaterialType.Opaque);
+            }
+        }
+    }
+}
