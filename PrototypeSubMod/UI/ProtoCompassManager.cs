d26794d289ea5296bfa328b7450e88fb1c11a54b
Indigocoder1 <caleb@liston.us>
2025-05-06 18:56:27 -0700

    Add compass to HUD

---
+++ PrototypeSubMod/UI/ProtoCompassManager.cs
@@ -0,0 +1,23 @@
+﻿using SubLibrary.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.UI;
+
+public class ProtoCompassManager : MonoBehaviour, IUIElement
+{
+    [SerializeField] private SubRoot subRoot;
+    [SerializeField] private Image compassImage;
+    [SerializeField] private Sprite[] cardinalSprites;
+    
+    public void UpdateUI()
+    {
+        int index = Mathf.RoundToInt((subRoot.transform.eulerAngles.y / 360) * 8);
+        compassImage.sprite = cardinalSprites[index];
+    }
+
+    public void OnSubDestroyed()
+    {
+        
+    }
+}
