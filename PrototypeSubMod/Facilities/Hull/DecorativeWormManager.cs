7284b31301675deb5120c4571c8fd6614ba40ed0
Indigocoder1 <caleb@liston.us>
2025-05-19 20:26:15 -0700

    Add decorative worm manager

---
+++ PrototypeSubMod/Facilities/Hull/DecorativeWormManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Hull;
+
+public class DecorativeWormManager : MonoBehaviour
+{
+    [SerializeField] private LightingController lightingController;
+    [SerializeField] private SkyApplier skyApplier;
+
+    private void Start()
+    {
+        lightingController.RegisterSkyApplier(skyApplier);
+        lightingController.SnapToState(0);
+    }
+}
