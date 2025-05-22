cb34bc649cab758d3cd46e70d4e5154115da34cf
Indigocoder1 <caleb@liston.us>
2025-05-21 18:56:47 -0700

    Add worm drill SFX path

---
+++ PrototypeSubMod/MiscMonobehaviors/FollowBezierPath.cs
@@ -0,0 +1,20 @@
+﻿using PrototypeSubMod.PathCreation;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class FollowBezierPath : MonoBehaviour
+{
+    [SerializeField] private PathCreator pathCreator;
+    [SerializeField] private float speed;
+
+    private float traveledDistance;
+    
+    private void Update()
+    {
+        traveledDistance += speed * Time.deltaTime;
+        
+        transform.position = pathCreator.path.GetPointAtDistance(traveledDistance);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(traveledDistance);
+    }
+}
