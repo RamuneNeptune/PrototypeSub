81b5da1408aca3632f3e276b89447866f7277342
Indigocoder1 <caleb@liston.us>
2025-05-22 14:08:14 -0700

    Add tech types to all worm variations

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/ApplyTagTechType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PrototypeSubMod.Utility;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class ApplyTagTechType : MonoBehaviour
+{
+    [SerializeField] private TechTag techTag;
+    [SerializeField] private DummyTechType techType;
+
+    private void OnValidate()
+    {
+        if (!techTag) TryGetComponent(out techTag);
+    }
+
+    private void Start()
+    {
+        techTag.type = techType.TechType;
+    }
+}
