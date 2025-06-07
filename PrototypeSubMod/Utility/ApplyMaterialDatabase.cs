14ab12be5611a66369b3751af779989ab11a26ab
Indigocoder1 <caleb@liston.us>
2025-06-06 20:14:44 -0700

    Fix black materials on worm

---
+++ PrototypeSubMod/Utility/ApplyMaterialDatabase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Utility;
+
+public class ApplyMaterialDatabase : MonoBehaviour
+{
+    [SerializeField] private GameObject applyTo;
+
+    private void OnValidate()
+    {
+        if (!applyTo) applyTo = gameObject;
+    }
+
+    private void Start()
+    {
+        UWE.CoroutineHost.StartCoroutine(ProtoMatDatabase.ReplaceVanillaMats(applyTo));
+    }
+}
