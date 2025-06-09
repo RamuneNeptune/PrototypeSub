e808babe131da7d58ecc2afdb679e803ca7e3a74
Indigocoder1 <caleb@liston.us>
2025-06-08 18:42:13 -0700

    Add player look animation during docking

--- PrototypeSubMod/Docking/ProtoDockingManager.cs
+++ PrototypeSubMod/Docking/ProtoDockingManager.cs
@@ -9,8 +9,10 @@ public class ProtoDockingManager : MonoBehaviour, IProtoEventListener, IProtoTre
     [SerializeField] private InterfloorTeleporter interfloorTeleporter;
     [SerializeField] private VehicleDockingBay dockingBay;
     [SerializeField] private IgnoreCinematicStart ignoreCinematicStart;
+    [SerializeField] private Transform playerPosition;
     [SerializeField] private Transform vehicleHolder;
 
+    private Vehicle lastDockedVehicle;
     private bool cinematicStartManaged;
     
     private void Start()
@@ -60,15 +62,37 @@ public class ProtoDockingManager : MonoBehaviour, IProtoEventListener, IProtoTre
         UWE.CoroutineHost.StartCoroutine(UndockDelayed());
     }
 
+    // Called via VehicleDockingBay Invoke
+    private void UnlockDoors()
+    {
+        if (!dockingBay.dockedVehicle || dockingBay.dockedVehicle == lastDockedVehicle) return;
+
+        lastDockedVehicle = dockingBay.dockedVehicle;
+        
+        var localPos = dockingBay.dockedVehicle.transform.InverseTransformPoint(dockingBay.dockedVehicle.playerPosition.transform.position);
+        playerPosition.localPosition = localPos;
+    }
+    
     private IEnumerator UndockDelayed()
     {
         FMODUWE.PlayOneShot(interfloorTeleporter.GetFMODAsset(), transform.position, 0.25f);
         InterfloorTeleporter.PlayTeleportEffect(0.2f);
 
         yield return new WaitForSeconds(0.1f);
-        
-        vehicleHolder.GetChild(0).gameObject.SetActive(true);
+
+        var vehicle = vehicleHolder.GetChild(0).gameObject;
         dockingBay.OnUndockingComplete(Player.main);
+        vehicle.SetActive(true);
+        
+        var rb = vehicle.GetComponent<Rigidbody>();
+        if (!rb) yield break;
+        
+        if (vehicle.GetComponent<Vehicle>().controlSheme == Vehicle.ControlSheme.Mech) yield break;
+        
+        yield return new WaitForSeconds(0.2f);
+        
+        rb.AddForce((rb.transform.forward - rb.transform.up) * 10f, ForceMode.VelocityChange);
+        rb.AddTorque(rb.transform.right * 2, ForceMode.VelocityChange);
     }
 
     public void OnProtoSerializeObjectTree(ProtobufSerializer serializer) { }
