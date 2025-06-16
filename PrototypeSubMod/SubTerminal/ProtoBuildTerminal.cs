69a99649d5a8080fc7fa34c8b4ac91a129ec9ab8
Indigocoder1 <caleb@liston.us>
2025-06-16 08:43:45 -0700

    Add additional log to build issue

--- PrototypeSubMod/SubTerminal/ProtoBuildTerminal.cs
+++ PrototypeSubMod/SubTerminal/ProtoBuildTerminal.cs
@@ -122,7 +122,8 @@ internal class ProtoBuildTerminal : Crafter
         yield return new WaitForSeconds(buildDuration + 0.2f);
         if (!constructing.isDone && !constructing.enabled)
         {
-            constructing.enabled = true;
+            ErrorMessage.AddError($"Sub isn't finished building. Fix needed");
+            Plugin.Logger.LogInfo("Sub isn't finished building. Fix needed");
         }
         
         Plugin.Logger.LogInfo("Sub should be finished building");
