c81fe06a93b799365d900baa0445b0b3e505ee38
Indigocoder1 <caleb@liston.us>
2025-06-15 20:19:40 -0700

    Add pedastal and door animation triggers

---
+++ PrototypeSubMod/Facilities/Engine/SecretDoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using PrototypeSubMod.Prefabs;
+using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Engine;
+
+public class SecretDoorTrigger : MonoBehaviour
+{
+    private static readonly int Door = Animator.StringToHash("OpenDoor");
+    
+    [SerializeField] private Animator animator;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.Equals(Player.mainCollider)) return;
+
+        var chip1 = Inventory.main.equipment.GetItemInSlot("Chip1");
+        var chip2 = Inventory.main.equipment.GetItemInSlot("Chip2");
+        if (chip1 == null && chip2 == null) return;
+
+        if (chip1 != null && chip1.techType == ListeningDevice_Craftable.prefabInfo.TechType)
+        {
+            OpenDoor();
+            return;
+        }
+
+        if (chip2 != null && chip2.techType == ListeningDevice_Craftable.prefabInfo.TechType)
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        animator.SetTrigger(Door);
+    }
+}
