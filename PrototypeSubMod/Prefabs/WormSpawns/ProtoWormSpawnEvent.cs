ef8bafcafa743f9fd18b031305c2f0caf47eef3c
Indigocoder1 <caleb@liston.us>
2025-06-18 15:25:50 -0700

    Rework worm spawn event registration

--- PrototypeSubMod/Prefabs/WormSpawns/ProtoWormSpawnEvent.cs
+++ PrototypeSubMod/Prefabs/WormSpawns/ProtoWormSpawnEvent.cs
@@ -18,17 +18,18 @@ public class ProtoWormSpawnEvent
         customPrefab.SetSpawns(spawnLocations);
         customPrefab.RemoveFromCache();
 
-        UWE.CoroutineHost.StartCoroutine(GetPrefab(prefab, customPrefab));
-        return prefabInfo;
-    }
+        IEnumerator GetPrefab(IOut<GameObject> prefabOut)
+        {
+            var instance = UWE.Utils.InstantiateDeactivated(prefab);
+            MaterialUtils.ApplySNShaders(instance);
+            yield return ProtoMatDatabase.ReplaceVanillaMats(instance);
 
-    private static IEnumerator GetPrefab(GameObject prefab, CustomPrefab customPrefab)
-    {
-        var instance = UWE.Utils.InstantiateDeactivated(prefab);
-        MaterialUtils.ApplySNShaders(instance);
-        yield return ProtoMatDatabase.ReplaceVanillaMats(instance);
-
-        customPrefab.SetGameObject(instance);
+            prefabOut.Set(instance);
+        }
+        
+        customPrefab.SetGameObject(GetPrefab);
         customPrefab.Register();
+        
+        return prefabInfo;
     }
 }
