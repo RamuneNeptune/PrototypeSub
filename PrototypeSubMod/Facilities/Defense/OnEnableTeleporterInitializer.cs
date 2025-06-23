55473d499617f204601f0f19b3287d2c7add6319
Indigocoder1 <caleb@liston.us>
2025-06-23 13:39:29 -0700

    Fix some engine facility teleporter issues

---
+++ PrototypeSubMod/Facilities/Defense/OnEnableTeleporterInitializer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Facilities.Defense;
+
+public class OnEnableTeleporterInitializer : MonoBehaviour
+{
+    [SerializeField] private PrecursorTeleporter teleporter;
+
+    private void OnEnable()
+    {
+        bool teleporterActive = TeleporterManager.GetTeleporterActive(teleporter.teleporterIdentifier);
+        teleporter.InitializeDoor(teleporterActive);
+    }
+}
