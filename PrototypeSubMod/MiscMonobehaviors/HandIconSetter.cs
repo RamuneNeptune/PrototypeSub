c5a254080912426c28aebcdb8c72c0bce2a88d54
Indigocoder1 <caleb@liston.us>
2025-06-24 18:50:36 -0700

    Add hand icon to first interact screen

---
+++ PrototypeSubMod/MiscMonobehaviors/HandIconSetter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class HandIconSetter : MonoBehaviour
+{
+    private bool mouseHovered;
+    
+    public void MouseEnter()
+    {
+        mouseHovered = true;
+    }
+
+    public void MouseExit()
+    {
+        mouseHovered = false;
+    }
+
+    private void Update()
+    {
+        if (!mouseHovered) return;
+        
+        HandReticle.main.SetIcon(HandReticle.IconType.Hand);
+        HandReticle.main.SetText(HandReticle.TextType.Hand, string.Empty, false);
+        HandReticle.main.SetText(HandReticle.TextType.HandSubscript, string.Empty, false);
+    }
+}
