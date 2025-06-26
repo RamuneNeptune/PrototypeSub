14d8e76276c77817572e8e26214c64ace26e0f0c
Indigocoder1 <caleb@liston.us>
2025-06-26 10:18:29 -0700

    Make stairs go up when outside sub

--- PrototypeSubMod/MiscMonobehaviors/SubSystems/ProtoStairsManager.cs
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/ProtoStairsManager.cs
@@ -5,14 +5,19 @@ namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
 public class ProtoStairsManager : MonoBehaviour
 {
     private static readonly int StairsActive = Animator.StringToHash("StairsActive");
-    
+
+    [SerializeField] private SubRoot subRoot;
     [SerializeField] private Animator stairsAnimator;
-    
+    [SerializeField] private FMOD_CustomEmitter stairsUpEmitter;
+    [SerializeField] private FMOD_CustomEmitter stairsDownEmitter;
+
+    private bool queuedNoPlaySFX;
     private bool stairsActive;
     
     private void Start()
     {
         Player.main.playerRespawnEvent.AddHandler(gameObject, OnRespawn);
+        SetStairsActive(Player.main.currentSub != subRoot);
     }
 
     private void OnRespawn(Player player)
@@ -29,5 +34,30 @@ public class ProtoStairsManager : MonoBehaviour
     public void ToggleStairsActive()
     {
         SetStairsActive(!stairsActive);
+    }
+
+    // Called by SubRoot.OnPlayerEntered
+    public void PlayerEnteredSub()
+    {
+        SetStairsActive(false);
+    }
+
+    // Called by SubRoot.OnPlayerExited
+    public void SaveEngineStateAndPowerDown()
+    {
+        queuedNoPlaySFX = true;
+        SetStairsActive(true);
+    }
+
+    public void PlayStairsUpSfx()
+    {
+        if (!queuedNoPlaySFX) stairsUpEmitter.Play();
+        queuedNoPlaySFX = false;
+    }
+    
+    public void PlayStairsDownSfx()
+    {
+        if (!queuedNoPlaySFX) stairsDownEmitter.Play();
+        queuedNoPlaySFX = false;
     }
 }
