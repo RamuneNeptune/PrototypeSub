a48639d4ecbd5fdd84f6c861282d8ee597bdbd69
Indigocoder1 <caleb@liston.us>
2025-06-29 16:53:14 -0700

    Rework how fin moving issues are handled

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/RigidbodyResetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class RigidbodyResetter : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+
+    private Vector3 centerOfMass;
+    private Vector3 inertiaTensor;
+    private Quaternion intertiaRotation;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+
+        centerOfMass = rigidbody.centerOfMass;
+        inertiaTensor = rigidbody.inertiaTensor;
+        intertiaRotation = rigidbody.inertiaTensorRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        if (inertiaTensor == Vector3.zero) return;
+
+        rigidbody.centerOfMass = centerOfMass;
+        rigidbody.inertiaTensor = inertiaTensor;
+        rigidbody.inertiaTensorRotation = intertiaRotation;
+    }
+}
