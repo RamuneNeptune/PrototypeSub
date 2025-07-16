0d8620705a62eb0aec2f357b014334f4034c629f
Indigocoder1 <caleb@liston.us>
2025-07-16 11:45:36 -0700

    Make PPT screen fade in when close

---
+++ PrototypeSubMod/MiscMonobehaviors/CanvasFadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class CanvasFadeIn : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float transitionSpeed = 1;
+
+    private float targetAlpha;
+
+    public void SetVisible()
+    {
+        targetAlpha = 1;
+    }
+
+    public void SetInvisible()
+    {
+        targetAlpha = 0;
+    }
+
+    private void Update()
+    {
+        if (Mathf.Approximately(canvasGroup.alpha, targetAlpha)) return;
+
+        canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetAlpha, Time.deltaTime * transitionSpeed);
+    }
+}
