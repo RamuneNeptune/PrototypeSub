d363c1cd9de16592dcb9a7bcbe391f988e530638
Indigocoder1 <caleb@liston.us>
2025-07-17 20:11:38 -0700

    Make ampeels take damage upon attacking sub

---
+++ PrototypeSubMod/MiscMonobehaviors/AmpeelAggroClearer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class AmpeelAggroClearer : MonoBehaviour, IOnTakeDamage
+{
+    public void OnTakeDamage(DamageInfo damageInfo)
+    {
+        if (!damageInfo.dealer) return;
+
+        var shocker = damageInfo.dealer.GetComponent<Shocker>();
+
+        if (!shocker) return;
+
+        shocker.liveMixin.TakeDamage(10);
+    }
+}
