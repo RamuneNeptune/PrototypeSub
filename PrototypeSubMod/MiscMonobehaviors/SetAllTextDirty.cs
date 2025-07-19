60cda33a5625f4e9bb96ca4837f1665a223a2be8
Indigocoder1 <caleb@liston.us>
2025-07-19 09:38:27 -0700

    Add set dirty call to fix weird text artifacts

---
+++ PrototypeSubMod/MiscMonobehaviors/SetAllTextDirty.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors;
+
+public class SetAllTextDirty : MonoBehaviour
+{
+    private TextMeshProUGUI[] texts;
+    
+    private void Start()
+    {
+        texts = GetComponentsInChildren<TextMeshProUGUI>(true);
+    }
+
+    public void SetDirty()
+    {
+        foreach (var text in texts)
+        {
+            text.SetAllDirty();
+        }
+    }
+}
