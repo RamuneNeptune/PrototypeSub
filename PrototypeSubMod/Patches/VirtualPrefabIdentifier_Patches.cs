92aa41c967605ff4a5a3e5256120527f836d4ab5
Indigocoder1 <caleb@liston.us>
2025-07-21 10:03:51 -0700

    Make items near moonpool door not spawn

---
+++ PrototypeSubMod/Patches/VirtualPrefabIdentifier_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(VirtualPrefabIdentifier))]
+public class VirtualPrefabIdentifier_Patches
+{
+    private static readonly Vector3 MOONPOOL_DOOR_POS = new(781, -323, -1050);
+    
+    [HarmonyPatch(nameof(VirtualPrefabIdentifier.Start)), HarmonyPrefix]
+    private static bool Start_Prefix(VirtualPrefabIdentifier __instance)
+    {
+        if ((__instance.transform.position - MOONPOOL_DOOR_POS).sqrMagnitude < 10000)
+        {
+            GameObject.Destroy(__instance.gameObject);
+            return false;
+        }
+
+        return true;
+    }
+}
