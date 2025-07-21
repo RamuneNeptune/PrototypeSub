0190bbab530ff55aa749fd0d55d21c6b02554115
Indigocoder1 <caleb@liston.us>
2025-07-21 10:10:53 -0700

    Remove reaper near defense facility door

---
+++ PrototypeSubMod/Patches/Creature_Patches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(Creature))]
+public class Creature_Patches
+{
+    [HarmonyPatch(nameof(Creature.Start)), HarmonyPrefix]
+    private static bool Start_Prefix(Creature __instance)
+    {
+        if (__instance is not ReaperLeviathan) return true;
+
+        var identifier = __instance.GetComponent<UniqueIdentifier>();
+        if (!identifier) return true;
+
+        // Remove that one Reaper near the defense moonpool door
+        if (identifier.Id == "d679ef73-3f07-495f-9f06-688123e3e806")
+        {
+            GameObject.Destroy(__instance.gameObject);
+            return false;
+        }
+
+        return true;
+    }
+}
