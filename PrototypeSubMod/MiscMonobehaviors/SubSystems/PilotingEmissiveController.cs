f15e407591618470315891ca5cdc49e382b163e0
Indigocoder1 <caleb@liston.us>
2025-07-23 10:36:06 -0700

    Make jaeger arm emissives turn on with engine

---
+++ PrototypeSubMod/MiscMonobehaviors/SubSystems/PilotingEmissiveController.cs
@@ -0,0 +1,21 @@
+﻿using PrototypeSubMod.EngineLever;
+using UnityEngine;
+
+namespace PrototypeSubMod.MiscMonobehaviors.SubSystems;
+
+public class PilotingEmissiveController : MonoBehaviour
+{
+    [SerializeField] private LightingController controller;
+    [SerializeField] private ProtoEngineLever engineLever;
+
+    private void Start()
+    {
+        engineLever.onEngineStateChanged += OnEngineChanged;
+        controller.emissiveController.RegisterRenderers(GetComponentsInChildren<Renderer>());
+    }
+
+    private void OnEngineChanged(bool engineOn)
+    {
+        controller.LerpToState(engineOn ? 1 : 0);
+    }
+}
