ab05bd6ba716e00fe7950963d379b476855cc462
OctolinkG <christianhowls@gmail.com>
2025-07-24 14:29:25 +0200

    Increment version

--- PrototypeSubMod/Plugin.cs
+++ PrototypeSubMod/Plugin.cs
@@ -41,7 +41,7 @@ namespace PrototypeSubMod
     {
         private const string GUID = "com.prototech.prototypesub";
         private const string pluginName = "Prototype Sub";
-        private const string versionString = "0.27.4";
+        private const string versionString = "0.28.0";
 
         public new static ManualLogSource Logger { get; private set; }
 
